da132a3bf1138768f5b0f8557e8dc134fbd94a96
Erik Hofman <erik@ehofman.com>
2012-01-12 10:35:12 +0100

    add some debugging defines and auto-wah parameters

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -39,6 +39,15 @@
 #define DEFAULT_IMA4_BLOCKSIZE		36
 #define IMA4_SMP_TO_BLOCKSIZE(a)	(((a)/2)+4)
 #define BLOCKSIZE_TO_SMP(a)		((a) > 1) ? (((a)-4)*2) : 1
+#define ENVELOPE_FOLLOW_STEP_CVT(a)	-0.1005f+pow((a), 0.25f)/3.15f;
+#ifndef NDEBUG
+# define DBG_MEMCLR(a, b, c, d)		if (a) memset((b), 0, (c)*(d))
+# define WRITE(a, b, dptr, ds, no_samples) \
+   if (a) { static int ct = 0; if (++ct > (b)) { \
+             WRITE_BUFFER_TO_FILE(dptr-ds, ds+no_samples); } }
+#else
+# define DBG_MEMCLR(a, b, c, d)
+#endif
 
 
 enum
@@ -263,9 +272,10 @@ typedef struct
 typedef struct
 {
    float coeff[4];
-   float k, lf_gain, hf_gain;
+   float fs, k, lf_gain, hf_gain;
    float freqfilter_history[_AAX_MAX_SPEAKERS][2];
    _oalRingBufferLFOInfo *lfo;
+   float lfo_param[4];
 } _oalRingBufferFreqFilterInfo;
 
 typedef struct
