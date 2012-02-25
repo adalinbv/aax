748fcd78b057f0c15a50ed1dc159496f4cf93ee5
Erik Hofman <erik@ehofman.com>
2012-02-25 10:55:10 +0100

    Remove unsued and useless code, print the capture device as well as the output device

--- test/testregisteredsensor.c
+++ test/testregisteredsensor.c
@@ -18,7 +18,7 @@
 #include "wavfile.h"
 
 #define REFRESH_RATE		  50.0f
-#define RECORD_TIME_SEC		  5.0f
+#define RECORD_TIME_SEC		6000.0f
 
 int main(int argc, char **argv)
 {
@@ -26,12 +26,16 @@ int main(int argc, char **argv)
    char *devname;
    int res;
 
-   devname = "AeonWave on File: /tmp/AeonWaveOut.wav";
-   printf("Play back the recorded audio buffers: %s\n", devname);
+   devname = getDeviceName(argc, argv);
+   if (!devname) {
+      devname = "AeonWave on File: /tmp/AeonWaveOut.wav";
+   }
+   printf("Playback: %s\n", devname);
    config = aaxDriverOpenByName(devname, AAX_MODE_WRITE_STEREO);
    testForError(config, "No default audio device available.");
 
-   devname = getDeviceName(argc, argv);
+   devname = getCaptureName(argc, argv);
+   printf("Recording: %s\n", devname);
    record = aaxDriverOpenByName(devname, AAX_MODE_READ);
    testForError(record, "Capture device is unavailable.");
 
@@ -40,18 +44,18 @@ int main(int argc, char **argv)
       enum aaxFormat format;
       aaxFrame frame;
       int channels;
-      float f, freq;
+      float f;
 
       format = AAX_PCM16S;
-      freq = 16000.0;
       channels = 2;
 
       /** mixer */
 #if 0
-      res = aaxMixerSetNoTracks(config, channels);
-      testForState(res, "aaxMixerSetNoTracks");
+      res = aaxMixerSetSetup(config, AAX_FREQUENCY, 192000);
+      testForState(res, "aaxMixerSeFrequency");
 #endif
-      res = aaxMixerInit(config);
+
+      res = aaxMixerSetState(config, AAX_INITIALIZED);
       testForState(res, "aaxMixerInit");
 
       res = aaxMixerSetState(config, AAX_PLAYING);
@@ -71,17 +75,12 @@ int main(int argc, char **argv)
 
       /** registered sensor */
       printf("Capturing %5.1f seconds of audio\n", RECORD_TIME_SEC);
-      res = aaxMixerSetSetup(record, AAX_FREQUENCY, freq);
-      testForState(res, "aaxMixerSeFrequency");
  
       res = aaxMixerSetSetup(record, AAX_TRACKS, channels);
       testForState(res, "aaxMixerSetNoTracks");
 
       res = aaxMixerSetSetup(record, AAX_FORMAT, format);
       testForState(res, "aaxMixerSetFormat");
-
-//    res = aaxMixerSetState(record, AAX_INITIALIZED);
-//    testForState(res, "aaxMixerSetInitialize");
 
       res = aaxAudioFrameRegisterSensor(frame, record);
       testForState(res, "aaxAudioFrameRegisterSensor");
@@ -95,29 +94,6 @@ int main(int argc, char **argv)
       f = 0.0f;
       do
       {
-#if 0
-         aaxBuffer buffer;
-         unsigned long ul;
-
-         res = aaxSensorWaitForBuffer(record, 3.0);
-         testForState(res, "aaxSensorWaitForBuffer");
-
-         ul = aaxSensorGetOffset(record, AAX_MICROSECONDS);
-         f = (float)ul * 1e-6;
-#if 1
-         printf("Record buffer position: %5.2f sec\r", f);
-#endif
-
-         buffer = aaxSensorGetBuffer(record);
-         testForError(buffer, "aaxSensorGetBuffer");
-
-         res = aaxEmitterAddBuffer(emitter, buffer);
-         testForState(res, "aaxEmitterAddBuffer");
-
-         res = aaxBufferDestroy(buffer);
-         testForState(res, "aaxBufferDestroy");
-#endif
-
          nanoSleep(7e7);
          f += 7e7*1e-9;
       }
