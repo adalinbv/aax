34f72b0cd784065d9f54c0b47b41afb2290438fa
Erik Hofman <erik@ehofman.com>
2012-03-09 14:37:14 +0100

    remove another debuging statement

--- src/alsasoft/device.c
+++ src/alsasoft/device.c
@@ -1372,7 +1372,6 @@ detect_devname(const char *devname, int devnum, unsigned int tracks, int m, char
 
       res = psnd_device_name_free_hint(hints);
    }
-printf("rv: %s\n", rv);
 
    return rv;
 }
