a000fb523d1620629605ebefe167f59ae6e4cdc5
Erik Hofman <erik@ehofman.com>
2012-12-20 14:57:15 +0100

    set the timer resolution to 1ms

--- src/aax_mixer.c
+++ src/aax_mixer.c
@@ -1114,7 +1114,7 @@ _aaxMixerStart(_handle_t *handle)
          {
             /* set the minimum timer resolution required by us to dt*1000 ms */
             handle->dt_ms = _oalRingBufferGetDuration(handle->ringbuffer)*1000;
-//          setTimerResolution(1); // 1 ms handle->dt_ms);
+            setTimerResolution(1); // 1 ms handle->dt_ms);
             rv = AAX_TRUE;
          }
       }   
@@ -1136,7 +1136,7 @@ _aaxMixerStop(_handle_t *handle)
    {
       if (handle-> dt_ms > 0)
       {
-//       resetTimerResolution(1); // 1ms handle->dt_ms);
+         resetTimerResolution(1); // 1ms handle->dt_ms);
          handle->dt_ms = 0;
       }
       handle->thread.started = AAX_FALSE;
