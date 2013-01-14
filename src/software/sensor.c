2431550112e87f5e306387d4cca75c0821e58e57
Erik Hofman <erik@ehofman.com>
2013-01-14 13:57:32 +0100

    slightly more efficient test, better resampling when frames < nframes and remove an obsolete line of code

--- src/software/sensor.c
+++ src/software/sensor.c
@@ -188,26 +188,31 @@ _aaxSensorCapture(_oalRingBuffer *dest_rb, const _aaxDriverBackend* be,
          assert(nrb != 0);
 
          tracks = rbd->no_tracks;
-         ds -= frames-nframes;
          for (t=0; t<tracks; t++)
          {
             int32_t *ptr = nrb->sample->track[t];
             int32_t *optr  = rbd->track[t];
 
-            /*
-             * The backend driver may fetch one sample more, or one sample
-             * less than requested to synchronize the capture and playback
-             * streams. If one extra sample is fetched it replaces the last
-             * sample in the delay effects buffer.
-             * If one sample less wat fetched the new data starts at position
-             * one and we have to construct the first sample in the buffer
-             * ourselves based ont the last sample in the delay effects buffer
-             * and the first new sample.
-             */
-            if (frames > nframes) {
-               *optr = (*(optr-1) + *(optr+1))/2;
-            } else if (frames < nframes) {
-               *(optr-1) = (*(optr-2) + *optr)/2;
+            if (frames != nframes)
+            {
+               /*
+                * The backend driver may fetch one sample more, or one sample
+                * less than requested to synchronize the capture and playback
+                * streams. If one extra sample is fetched it replaces the last
+                * sample in the delay effects buffer.
+                * If one sample less wat fetched the new data starts at the
+                * second position and we have to construct the first sample in
+                * the buffer ourselves based on the last sample in the delay
+                * effects buffer and the first new sample.
+                */
+               if (frames < nframes)
+               {
+                  *(optr-1) = (*(optr-2)*2 + *(optr+1))/3;
+                  *optr     = (*(optr-2) + *(optr+1)*2)/3;
+               }
+               else if (frames > nframes) {
+                  *optr = (*(optr-1) + *(optr+1))/2;
+               }
             }
 
             _aax_memcpy(ptr-ds, optr-ds+nframes, ds*bps);
