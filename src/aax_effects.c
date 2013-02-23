d862282a1d9cd87db8e9622abf32453d0020ca84
Erik Hofman <erik@ehofman.com>
2013-02-23 14:03:01 +0100

    Allow upt to +12dB gain increase for distortion

--- src/aax_effects.c
+++ src/aax_effects.c
@@ -918,7 +918,7 @@ static const _eff_minmax_tbl_t _eff_minmax_tbl[_MAX_SLOTS][AAX_EFFECT_MAX] =
     /* AAX_TIMED_PITCH_EFFECT */
     { {  0.0f, 0.0f, 0.0f, 0.0f }, {     4.0f, MAXFLOAT, 4.0f, MAXFLOAT } },
     /* AAX_DISTORTION_EFFECT */
-    { {  0.0f, 0.0f, 0.0f, 0.0f }, {     2.0f,     1.0f, 1.0f,     1.0f } },
+    { {  0.0f, 0.0f, 0.0f, 0.0f }, {     4.0f,     1.0f, 1.0f,     1.0f } },
     /* AAX_PHASING_EFFECT   */
     { { 0.0f, 0.01f, 0.0f, 0.0f }, {     1.0f,    10.0f, 1.0f,     1.0f } },
     /* AAX_CHORUS_EFFECT    */
