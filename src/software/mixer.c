2811d879fa5d96d486ff5d0e2caf70e5b6d87b9f
Erik Hofman <erik@ehofman.com>
2013-08-28 12:59:44 +0200

    fix frame delay effects (e.g. for aaxfx)

--- src/software/mixer.c
+++ src/software/mixer.c
@@ -33,7 +33,6 @@ _aaxSoftwareMixerApplyEffects(const void *id, const void *hid, void *drb, const 
    _oalRingBufferDelayEffectData* delay_effect;
    _oalRingBufferFreqFilterInfo* freq_filter;
    _oalRingBufferSample *rbd;
-   int32_t **track_ptr;
    float maxgain, gain;
    int dist_state;
 
@@ -41,8 +40,6 @@ _aaxSoftwareMixerApplyEffects(const void *id, const void *hid, void *drb, const 
    assert(rb->sample != 0);
 
    rbd = rb->sample;
-   track_ptr = (int32_t**)rbd->scratch;
-
    assert(rbd->bytes_sample == sizeof(int32_t));
 
    delay_effect = _EFFECT_GET_DATA(p2d, DELAY_EFFECT);
@@ -50,11 +47,11 @@ _aaxSoftwareMixerApplyEffects(const void *id, const void *hid, void *drb, const 
    dist_state = _EFFECT_GET_STATE(p2d, DISTORTION_EFFECT);
    if (delay_effect || freq_filter || dist_state)
    {
-      int32_t *scratch0 = track_ptr[SCRATCH_BUFFER0];
-      int32_t *scratch1 = track_ptr[SCRATCH_BUFFER1];
+      int32_t *scratch0 = rbd->scratch[SCRATCH_BUFFER0];
+      int32_t *scratch1 = rbd->scratch[SCRATCH_BUFFER1];
       void* distortion_effect = NULL;
       unsigned int no_samples, ddesamps = 0;
-      unsigned int track, tracks;
+      unsigned int track, tracks, bps;
 
       if (dist_state) {
          distortion_effect = &p2d->effect[DISTORTION_EFFECT];
@@ -70,6 +67,7 @@ _aaxSoftwareMixerApplyEffects(const void *id, const void *hid, void *drb, const 
          ddesamps = (unsigned int)ceilf(DELAY_EFFECTS_TIME*rbd->frequency_hz);
       }
 
+      bps = rbd->bytes_sample;
       tracks = rbd->no_tracks;
       no_samples = rbd->no_samples;
       for (track=0; track<tracks; track++)
@@ -77,21 +75,23 @@ _aaxSoftwareMixerApplyEffects(const void *id, const void *hid, void *drb, const 
          int32_t *dptr = rbd->track[track];
          int32_t *ddeptr = dptr - ddesamps;
 
-         /* save the unmodified next effects buffer for later use            */
-         /* (scratch buffers have a leading *and* a trailing effects buffer) */
-         DBG_MEMCLR(1,scratch1-ddesamps, no_samples+2*ddesamps,sizeof(int32_t));
-         _aax_memcpy(scratch1+no_samples, ddeptr+no_samples,
-                     ddesamps*sizeof(int32_t));
+         /* save the unmodified next effects buffer for later use          */
+         /* (scratch buffers have a leading and a trailing effects buffer) */
+         DBG_MEMCLR(1, scratch1-ddesamps, no_samples+2*ddesamps, bps);
+         _aax_memcpy(scratch1+no_samples, ddeptr+no_samples, ddesamps*bps);
 
          /* mix the buffer and the delay buffer */
-         _aax_memcpy(scratch1, dptr, no_samples*sizeof(int32_t));
-         bufEffectsApply(dptr, scratch1, scratch0, 0, no_samples,
-                         no_samples, ddesamps, track, 0,
+         DBG_MEMCLR(1, scratch0-ddesamps, no_samples+2*ddesamps, bps);
+         bufEffectsApply(scratch0, dptr, scratch1,
+                         0, no_samples, no_samples, ddesamps, track, 0,
                          freq_filter, delay_effect, distortion_effect);
 
          /* copy the unmodified next effects buffer back */
-         DBG_MEMCLR(1, dptr-ddesamps, no_samples+ddesamps, sizeof(int32_t));
-         _aax_memcpy(ddeptr, scratch1+no_samples, ddesamps*sizeof(int32_t));
+         DBG_MEMCLR(1, dptr-ddesamps, no_samples+ddesamps, bps);
+         _aax_memcpy(ddeptr, scratch1+no_samples, ddesamps*bps);
+
+         /* copy the data back from scratch0 to dptr */
+         _aax_memcpy(dptr, scratch0, no_samples*bps);
       }
    }
 
