c53b868bfd44a898d9c2e9b512a79d43a9f16eb3
Erik Hofman <erik@ehofman.com>
2013-12-18 11:41:57 +0100

    split up rbuf_mixsingle.c in a 2d and a 3d part

--- src/software/cpu2d/rbuf_mixsingle.c
+++ src/software/cpu2d/rbuf_mixsingle.c
@@ -38,18 +38,6 @@
 #include "arch_simd.h"
 #include "software/ringbuffer.h"
 
-/* Forward declartations */
-static _aaxRingBufferDistFn _aaxRingBufferDistNone;
-static _aaxRingBufferDistFn _aaxRingBufferDistInvExp;
-static _aaxRingBufferPitchShiftFn _aaxRingBufferDopplerShift;
-
-static _aaxRingBufferDistFn _aaxRingBufferALDistInv;
-static _aaxRingBufferDistFn _aaxRingBufferALDistInvClamped;
-static _aaxRingBufferDistFn _aaxRingBufferALDistLin;
-static _aaxRingBufferDistFn _aaxRingBufferALDistLinClamped;
-static _aaxRingBufferDistFn _aaxRingBufferALDistExp;
-static _aaxRingBufferDistFn _aaxRingBufferALDistExpClamped;
-
 /**
  * Mix a single track source buffer into a multi track destination buffer.
  * The result will be a stereo-mixed multitrack recording.
@@ -206,28 +194,6 @@ _aaxRingBufferMixMono16(_aaxRingBuffer *drb, _aaxRingBuffer *srb, _aaxRingBuffer
 
 /* -------------------------------------------------------------------------- */
 
-_aaxRingBufferDistFn *_aaxRingBufferDistanceFn[AAX_DISTANCE_MODEL_MAX] =
-{
-   (_aaxRingBufferDistFn *)&_aaxRingBufferDistNone,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferDistInvExp
-};
-
-#define AL_DISTANCE_MODEL_MAX AAX_AL_DISTANCE_MODEL_MAX-AAX_AL_INVERSE_DISTANCE
-_aaxRingBufferDistFn *_aaxRingBufferALDistanceFn[AL_DISTANCE_MODEL_MAX] =
-{
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistInv,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistInvClamped,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistLin,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistLinClamped,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistExp,
-   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistExpClamped
-};
-
-_aaxRingBufferPitchShiftFn *_aaxRingBufferDopplerFn[] =
-{
-   (_aaxRingBufferPitchShiftFn *)&_aaxRingBufferDopplerShift
-};
-
 void
 _aaxRingBufferMixMono16Stereo(_aaxRingBufferData *drbi, const int32_ptrptr sptr, _aaxRingBuffer2dProps *ep2d, unsigned char ch, unsigned int offs, unsigned int dno_samples, float gain, float svol, float evol)
 {
@@ -342,7 +308,6 @@ _aaxRingBufferMixMono16Spatial(_aaxRingBufferData *drbi, int32_t **sptr, _aaxRin
    unsigned int t;
 
    _AAX_LOG(LOG_DEBUG, __FUNCTION__);
-
    /** Mix */
    rbd = drbi->sample;
    for (t=0; t<rbd->no_tracks; t++)
@@ -443,123 +408,3 @@ _aaxRingBufferMixMono16HRTF(_aaxRingBufferData *drbi, int32_t **sptr, _aaxRingBu
    }
 }
 
-static float
-_aaxRingBufferDistNone(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   return 1.0f;
-}
-
-
-/**
- * http://www.engineeringtoolbox.com/outdoor-propagation-sound-d_64.html
- *
- * Lp = Lw + 10 log(Q/(4π r2) + 4/R)  (1b)
- *
- * where
- *
- * Lp = sound pressure level (dB)
- * Lw = sound power level source in decibel (dB)
- * Q = Q coefficient 
- *     1 if uniform spherical
- *     2 if uniform half spherical (single reflecting surface)
- *     4 if uniform radiation over 1/4 sphere (two reflecting surfaces, corner)
- * r = distance from source   (m)
- * R = room constant (m2)
- */
-static float
-_aaxRingBufferDistInvExp(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-#if 1
-   float fraction = 0.0f, gain = 1.0f;
-   if (ref_dist) fraction = _MAX(dist, 0.01f) / _MAX(ref_dist, 0.01f);
-   if (fraction) gain = powf(fraction, -rolloff);
-   return gain;
-#else
-   return powf(dist/ref_dist, -rolloff);
-#endif
-}
-
-static float
-_aaxRingBufferDopplerShift(float vs, float ve, float vsound)
-{
-#if 1
-   float vse, rv;
-
-   /* relative speed */
-   vse = _MIN(ve, vsound) - _MIN(vs, vsound);
-   rv =  vsound/_MAX(vsound - vse, 1.0f);
-
-   return rv;
-#else
-   float vss, ves;
-   vss = vsound - _MIN(vs, vsound);
-   ves = _MAX(vsound - _MIN(ve, vsound), 1.0f);
-   return vss/ves;
-#endif
-}
-
-
-/* --- OpenAL support --- */
-
-static float
-_aaxRingBufferALDistInv(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float gain = 1.0f;
-   float denom = ref_dist + rolloff * (dist - ref_dist);
-   if (denom) gain = ref_dist/denom;
-   return gain;
-}
-
-static float
-_aaxRingBufferALDistInvClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float gain = 1.0f;
-   float denom;
-   dist = _MAX(dist, ref_dist);
-   dist = _MIN(dist, max_dist);
-   denom = ref_dist + rolloff * (dist - ref_dist);
-   if (denom) gain = ref_dist/denom;
-   return gain;
-}
-
-static float
-_aaxRingBufferALDistLin(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float gain = 1.0f;
-   float denom = max_dist - ref_dist;
-   if (denom) gain = (1-rolloff)*(dist-ref_dist)/denom;
-   return gain;
-}
-
-static float
-_aaxRingBufferALDistLinClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float gain = 1.0f;
-   float denom = max_dist - ref_dist;
-   dist = _MAX(dist, ref_dist);
-   dist = _MIN(dist, max_dist);
-   if (denom) gain = (1-rolloff)*(dist-ref_dist)/denom;
-   return gain;
-}
-
-static float
-_aaxRingBufferALDistExp(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float fraction = 0.0f, gain = 1.0f;
-   if (ref_dist) fraction = dist / ref_dist;
-   if (fraction) gain = powf(fraction, -rolloff);
-   return gain;
-}
-
-static float
-_aaxRingBufferALDistExpClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
-{
-   float fraction = 0.0f, gain = 1.0f;
-
-   dist = _MAX(dist, ref_dist);
-   dist = _MIN(dist, max_dist);
-   if (ref_dist) fraction = dist / ref_dist;
-   if (fraction) gain = powf(fraction, -rolloff);
-
-   return gain;
-}
---
+++ src/software/cpu3d/rbuf_mixsingle.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2005-2013 by Erik Hofman.
+ * Copyright 2009-2013 by Adalin B.V.
+ * All Rights Reserved.
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Adalin B.V.;
+ * the contents of this file may not be disclosed to third parties, copied or
+ * duplicated in any form, in whole or in part, without the prior written
+ * permission of Adalin B.V.
+ */
+
+/*
+ * 1:N ringbuffer mixer functions.
+ */
+
+/*
+ * Sources:
+ * http://en.wikipedia.org/wiki/Doppler_effect
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "software/ringbuffer.h"
+
+/* Forward declartations */
+static _aaxRingBufferDistFn _aaxRingBufferDistNone;
+static _aaxRingBufferDistFn _aaxRingBufferDistInvExp;
+static _aaxRingBufferPitchShiftFn _aaxRingBufferDopplerShift;
+
+static _aaxRingBufferDistFn _aaxRingBufferALDistInv;
+static _aaxRingBufferDistFn _aaxRingBufferALDistInvClamped;
+static _aaxRingBufferDistFn _aaxRingBufferALDistLin;
+static _aaxRingBufferDistFn _aaxRingBufferALDistLinClamped;
+static _aaxRingBufferDistFn _aaxRingBufferALDistExp;
+static _aaxRingBufferDistFn _aaxRingBufferALDistExpClamped;
+
+/* -------------------------------------------------------------------------- */
+
+_aaxRingBufferDistFn *_aaxRingBufferDistanceFn[AAX_DISTANCE_MODEL_MAX] =
+{
+   (_aaxRingBufferDistFn *)&_aaxRingBufferDistNone,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferDistInvExp
+};
+
+#define AL_DISTANCE_MODEL_MAX AAX_AL_DISTANCE_MODEL_MAX-AAX_AL_INVERSE_DISTANCE
+_aaxRingBufferDistFn *_aaxRingBufferALDistanceFn[AL_DISTANCE_MODEL_MAX] =
+{
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistInv,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistInvClamped,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistLin,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistLinClamped,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistExp,
+   (_aaxRingBufferDistFn *)&_aaxRingBufferALDistExpClamped
+};
+
+_aaxRingBufferPitchShiftFn *_aaxRingBufferDopplerFn[] =
+{
+   (_aaxRingBufferPitchShiftFn *)&_aaxRingBufferDopplerShift
+};
+
+static float
+_aaxRingBufferDistNone(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   return 1.0f;
+}
+
+
+/**
+ * http://www.engineeringtoolbox.com/outdoor-propagation-sound-d_64.html
+ *
+ * Lp = Lw + 10 log(Q/(4π r2) + 4/R)  (1b)
+ *
+ * where
+ *
+ * Lp = sound pressure level (dB)
+ * Lw = sound power level source in decibel (dB)
+ * Q = Q coefficient 
+ *     1 if uniform spherical
+ *     2 if uniform half spherical (single reflecting surface)
+ *     4 if uniform radiation over 1/4 sphere (two reflecting surfaces, corner)
+ * r = distance from source   (m)
+ * R = room constant (m2)
+ */
+static float
+_aaxRingBufferDistInvExp(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+#if 1
+   float fraction = 0.0f, gain = 1.0f;
+   if (ref_dist) fraction = _MAX(dist, 0.01f) / _MAX(ref_dist, 0.01f);
+   if (fraction) gain = powf(fraction, -rolloff);
+   return gain;
+#else
+   return powf(dist/ref_dist, -rolloff);
+#endif
+}
+
+static float
+_aaxRingBufferDopplerShift(float vs, float ve, float vsound)
+{
+#if 1
+   float vse, rv;
+
+   /* relative speed */
+   vse = _MIN(ve, vsound) - _MIN(vs, vsound);
+   rv =  vsound/_MAX(vsound - vse, 1.0f);
+
+   return rv;
+#else
+   float vss, ves;
+   vss = vsound - _MIN(vs, vsound);
+   ves = _MAX(vsound - _MIN(ve, vsound), 1.0f);
+   return vss/ves;
+#endif
+}
+
+
+/* --- OpenAL support --- */
+
+static float
+_aaxRingBufferALDistInv(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float gain = 1.0f;
+   float denom = ref_dist + rolloff * (dist - ref_dist);
+   if (denom) gain = ref_dist/denom;
+   return gain;
+}
+
+static float
+_aaxRingBufferALDistInvClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float gain = 1.0f;
+   float denom;
+   dist = _MAX(dist, ref_dist);
+   dist = _MIN(dist, max_dist);
+   denom = ref_dist + rolloff * (dist - ref_dist);
+   if (denom) gain = ref_dist/denom;
+   return gain;
+}
+
+static float
+_aaxRingBufferALDistLin(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float gain = 1.0f;
+   float denom = max_dist - ref_dist;
+   if (denom) gain = (1-rolloff)*(dist-ref_dist)/denom;
+   return gain;
+}
+
+static float
+_aaxRingBufferALDistLinClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float gain = 1.0f;
+   float denom = max_dist - ref_dist;
+   dist = _MAX(dist, ref_dist);
+   dist = _MIN(dist, max_dist);
+   if (denom) gain = (1-rolloff)*(dist-ref_dist)/denom;
+   return gain;
+}
+
+static float
+_aaxRingBufferALDistExp(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float fraction = 0.0f, gain = 1.0f;
+   if (ref_dist) fraction = dist / ref_dist;
+   if (fraction) gain = powf(fraction, -rolloff);
+   return gain;
+}
+
+static float
+_aaxRingBufferALDistExpClamped(float dist, float ref_dist, float max_dist, float rolloff, float vsound, float Q)
+{
+   float fraction = 0.0f, gain = 1.0f;
+
+   dist = _MAX(dist, ref_dist);
+   dist = _MIN(dist, max_dist);
+   if (ref_dist) fraction = dist / ref_dist;
+   if (fraction) gain = powf(fraction, -rolloff);
+
+   return gain;
+}
