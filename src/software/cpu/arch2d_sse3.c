a74ca41004c373728664498e84e3517d6559f7c8
Erik Hofman <erik@ehofman.com>
2013-12-30 10:55:45 +0100

    15% speed improvement for SSE3 Cubic resampling, it's now on par with SSE2 pure C implementation

--- src/software/cpu/arch2d_sse3.c
+++ src/software/cpu/arch2d_sse3.c
@@ -92,6 +92,7 @@ _batch_fmul_value_sse3(void* data, unsigned bps, unsigned int num, float f)
    }
 }
 
+/** NOTE: instruction sequence is important for execution speed! */
 void
 _aaxBufResampleCubic_sse3(int32_ptr d, const_int32_ptr s, unsigned int dmin, unsigned int dmax, unsigned int sdesamps, float smu, float freq_factor)
 {
@@ -99,11 +100,14 @@ _aaxBufResampleCubic_sse3(int32_ptr d, const_int32_ptr s, unsigned int dmin, uns
    const __m128 y1m = _mm_set_ps(-1.0f, 0.0f, 1.0f, 0.0f);
    const __m128 y2m = _mm_set_ps( 2.0f,-2.0f, 1.0f,-1.0f);
    const __m128 y3m = _mm_set_ps(-1.0f, 1.0f,-1.0f, 1.0f);
-   __m128 y0123, a0123, vm0, vm1, vm2, vm3;
+   __m128 y0123, a0123, xsmu;
+   __m128 vm0, vm1, vm2, vm3;
+   __m128 xtmp, xtmp1, xtmp2;
    __m128i xmm0i;
    int32_ptr sptr = (int32_ptr)s;
    int32_ptr dptr = d;
    unsigned int i;
+   float smu2;
 
    assert(s != 0);
    assert(d != 0);
@@ -120,59 +124,74 @@ _aaxBufResampleCubic_sse3(int32_ptr d, const_int32_ptr s, unsigned int dmin, uns
       xmm0i = _mm_loadu_si128((__m128i*)sptr);
    }
    y0123 = _mm_cvtepi32_ps(xmm0i);
-   sptr += 4;
 
+   /* 
+    * a0 = y3 - y2 - y0 + y1;
+    * a1 = y0 - y1 - a0;
+    * a2 = y2 - y0;
+    * a3 = y1;
+    */
    vm0 = _mm_mul_ps(y0123, y0m);
    vm1 = _mm_mul_ps(y0123, y1m);
+   xtmp = _mm_hadd_ps(vm0, vm1);
+
+   sptr += 4;
+
    vm2 = _mm_mul_ps(y0123, y2m);
    vm3 = _mm_mul_ps(y0123, y3m);
-   a0123 = _mm_hadd_ps(_mm_hadd_ps(vm0, vm1), _mm_hadd_ps(vm2, vm3));
+   xtmp1 = _mm_hadd_ps(vm2, vm3);
 
+   /* work in advance */
+   smu2 = smu*smu;
+   xsmu = _mm_set_ps(smu*smu*smu, smu*smu, smu, 1.0f);
+
+   a0123 = _mm_hadd_ps(xtmp, xtmp1);
+   y0123 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(y0123),
+                                                    sizeof(float)));
    i = dmax-dmin;
    if (i)
    {
       do
       {
-         __m128 xsmu, xtmp;
-         float smu2 = smu*smu;
-#if 0
-         ftmp = (a0*smu*smu2 + a1*smu2 + a2*smu + a3);
-         *dptr++ = (int32_t)ftmp;
-#else
-         xsmu = _mm_set_ps(smu*smu2, smu2, smu, 1.0f);
          xtmp = _mm_mul_ps(a0123, xsmu);
-         xtmp = _mm_hadd_ps(_mm_hadd_ps(xtmp, xtmp), _mm_hadd_ps(xtmp, xtmp));
-         *dptr++ = _mm_cvttss_si32(xtmp);
-#endif
+         xtmp = _mm_hadd_ps(xtmp, xtmp);
 
          smu += freq_factor;
+
+         xtmp = _mm_hadd_ps(xtmp, xtmp);
+
          if (smu >= 1.0)
          {
-            smu--;
-
-#if 0
-            y0 = y1;
-            y1 = y2;
-            y2 = y3;
-            y3 = (float)*sptr++;
-#endif
-            y0123 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(y0123),
-                                                    sizeof(float)));
             y0123 = _mm_cvtsi32_ss(y0123, *sptr++);
 
-#if 0
-            a0 = y3 - y2 - y0 + y1;
-            a1 = y0 - y1 - a0;
-            a2 = y2 - y0;
-            a3 = y1;
-#else
+            /* 
+             * a0 = y3 - y2 - y0 + y1;
+             * a1 = y0 - y1 - a0;
+             * a2 = y2 - y0;
+             * a3 = y1;
+             */
             vm0 = _mm_mul_ps(y0123, y0m);
             vm1 = _mm_mul_ps(y0123, y1m);
+            xtmp1 = _mm_hadd_ps(vm0, vm1);
+
+            smu--;
+
             vm2 = _mm_mul_ps(y0123, y2m);
             vm3 = _mm_mul_ps(y0123, y3m);
-            a0123 = _mm_hadd_ps(_mm_hadd_ps(vm0, vm1), _mm_hadd_ps(vm2, vm3));
-#endif
+            xtmp2 = _mm_hadd_ps(vm2, vm3);
+
+            /* work in advance */
+            smu2 = smu*smu;
+            xsmu = _mm_set_ps(smu*smu2, smu2, smu, 1.0f);
+
+            a0123 = _mm_hadd_ps(xtmp1, xtmp2);
+            y0123 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(y0123),
+                                                    sizeof(float)));
          }
+
+         xsmu = _mm_set_ps(smu*smu2, smu2, smu, 1.0f);
+
+         *dptr++ = _mm_cvttss_si32(xtmp);
       }
       while (--i);
    }
