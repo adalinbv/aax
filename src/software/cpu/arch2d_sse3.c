286b02e50796e2d8f81d8963e5f1720dbeaf8b59
Erik Hofman <erik@ehofman.com>
2014-01-26 13:52:05 +0100

    Implement _batch_resample_float_sse3

--- src/software/cpu/arch2d_sse3.c
+++ src/software/cpu/arch2d_sse3.c
@@ -215,6 +215,130 @@ _batch_resample_sse3(int32_ptr d, const_int32_ptr s, unsigned int dmin, unsigned
    } else {
       _aaxBufResampleNearest_sse2(d, s, dmin, dmax, 0, smu, fact);
    }
+} 
+
+
+static void
+_aaxBufResampleCubic_float_sse3(float32_ptr d, const_float32_ptr s, unsigned int dmin, unsigned int dmax, unsigned int sdesamps, float smu, float freq_factor)
+{
+const __m128 y0m = _mm_set_ps( 0.0f, 1.0f, 0.0f, 0.0f);
+   const __m128 y1m = _mm_set_ps(-1.0f, 0.0f, 1.0f, 0.0f);
+   const __m128 y2m = _mm_set_ps( 2.0f,-2.0f, 1.0f,-1.0f);
+   const __m128 y3m = _mm_set_ps(-1.0f, 1.0f,-1.0f, 1.0f);
+   __m128 y0123, a0123, xsmu;
+   __m128 vm0, vm1, vm2, vm3;
+   __m128 xtmp, xtmp1, xtmp2;
+   float32_ptr sptr = (float32_ptr)s;
+   float32_ptr dptr = d;
+   unsigned int i;
+   float smu2;
+
+   assert(s != 0);
+   assert(d != 0);
+   assert(dmin < dmax);
+   assert(0.0f <= smu && smu < 1.0f);
+   assert(0.0f < freq_factor && freq_factor <= 1.0f);
+
+   sptr += sdesamps;
+   dptr += dmin;
+
+   if (((size_t)sptr & 0xF) == 0) {
+      y0123 = _mm_load_ps((float*)sptr);
+   } else {
+      y0123 = _mm_loadu_ps((float*)sptr);
+   }
+
+   /* 
+    * a0 = y3 - y2 - y0 + y1;
+    * a1 = y0 - y1 - a0;
+    * a2 = y2 - y0;
+    * a3 = y1;
+    */
+   vm0 = _mm_mul_ps(y0123, y0m);
+   vm1 = _mm_mul_ps(y0123, y1m);
+   xtmp = _mm_hadd_ps(vm0, vm1);
+
+   sptr += 4;
+
+   vm2 = _mm_mul_ps(y0123, y2m);
+   vm3 = _mm_mul_ps(y0123, y3m);
+   xtmp1 = _mm_hadd_ps(vm2, vm3);
+
+   /* work in advance */
+   smu2 = smu*smu;
+   xsmu = _mm_set_ps(smu*smu*smu, smu*smu, smu, 1.0f);
+
+   a0123 = _mm_hadd_ps(xtmp, xtmp1);
+   y0123 = _mm_shuffle_ps(y0123, y0123, _MM_SHUFFLE(2, 1, 0, 0));
+   i = dmax-dmin;
+   if (i)
+   {
+      do
+      {
+         xtmp = _mm_mul_ps(a0123, xsmu);
+         xtmp = _mm_hadd_ps(xtmp, xtmp);
+
+         smu += freq_factor;
+
+         xtmp = _mm_hadd_ps(xtmp, xtmp);
+
+         if (smu >= 1.0)
+         {
+            y0123 = _mm_move_ss(y0123, _mm_load_ss((float*)sptr++));
+
+            /* 
+             * a0 = y3 - y2 - y0 + y1;
+             * a1 = y0 - y1 - a0;
+             * a2 = y2 - y0;
+             * a3 = y1;
+             */
+            vm0 = _mm_mul_ps(y0123, y0m);
+            vm1 = _mm_mul_ps(y0123, y1m);
+            xtmp1 = _mm_hadd_ps(vm0, vm1);
+            smu--;
+
+            vm2 = _mm_mul_ps(y0123, y2m);
+            vm3 = _mm_mul_ps(y0123, y3m);
+            xtmp2 = _mm_hadd_ps(vm2, vm3);
+
+            /* work in advance */
+            smu2 = smu*smu;
+            xsmu = _mm_set_ps(smu*smu2, smu2, smu, 1.0f);
+
+            a0123 = _mm_hadd_ps(xtmp1, xtmp2);
+            y0123 = _mm_shuffle_ps(y0123, y0123, _MM_SHUFFLE(2, 1, 0, 3));
+         }
+
+         xsmu = _mm_set_ps(smu*smu2, smu2, smu, 1.0f);
+
+         _mm_store_ss((float*)dptr++, xtmp);
+      }
+      while (--i);
+   }
 }
+
+
+void _aaxBufResampleLinear_float_sse2(float32_ptr, const_float32_ptr, unsigned int, unsigned int, unsigned int, float, float);
+void _aaxBufResampleNearest_float_sse2(float32_ptr, const_float32_ptr, unsigned int, unsigned int, unsigned int, float, float);
+void _aaxBufResampleSkip_float_sse2(float32_ptr, const_float32_ptr, unsigned int, unsigned int, unsigned int, float, float);
+
+void
+_batch_resample_float_sse3(float32_ptr d, const_float32_ptr s, unsigned int dmin, unsigned int dmax, float smu, float fact)
+{
+   assert(fact > 0.0f);
+
+   if (fact < CUBIC_TRESHOLD) {
+      _aaxBufResampleCubic_float_sse3(d, s, dmin, dmax, 0, smu, fact);
+   }
+   else if (fact < 1.0f) {
+      _aaxBufResampleLinear_float_sse2(d, s, dmin, dmax, 0, smu, fact);
+   }
+   else if (fact > 1.0f) {
+      _aaxBufResampleSkip_float_sse2(d, s, dmin, dmax, 0, smu, fact);
+   } else {
+      _aaxBufResampleNearest_float_sse2(d, s, dmin, dmax, 0, smu, fact);
+   }
+}
+
 #endif /* SSE3 */
 
