23bb87c51d1d81534f62e256f7a0d5e9453e5f06
Erik Hofman <erik@ehofman.com>
2015-05-04 13:25:28 +0200

    testing for (v < GMATH_128DB) is not enough, also make sure (vstep < GMATH_128DB)

--- src/software/cpu/arch2d_sse2.c
+++ src/software/cpu/arch2d_sse2.c
@@ -499,7 +499,7 @@ _batch_imadd_sse2(int32_ptr dst, const_int32_ptr src, size_t num, float v, float
    int32_ptr s = (int32_ptr)src;
    size_t i, step, dtmp, stmp;
 
-   if (!num || v < GMATH_128DB) return;
+   if (!num || (v < GMATH_128DB && vstep < GMATH_128DB)) return;
    if (fabs(v - 1.0f) <GMATH_128DB && vstep < GMATH_128DB) {
       _batch_iadd_sse2(dst, src, num);
       return;
@@ -715,7 +715,7 @@ _batch_fmadd_sse2(float32_ptr dst, const_float32_ptr src, size_t num, float v, f
    float32_ptr d = (float32_ptr)dst;
    size_t i, step, dtmp, stmp;
 
-   if (!num || v < GMATH_128DB) return;
+   if (!num || (v < GMATH_128DB && vstep < GMATH_128DB)) return;
    if (fabs(v - 1.0f) <GMATH_128DB && vstep < GMATH_128DB) {
       _batch_fadd_sse2(dst, src, num);
       return;
