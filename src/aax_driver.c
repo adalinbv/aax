c2352041dc28d37014b78951bdf58d19e09663a8
Erik Hofman <erik@ehofman.com>
2015-05-21 12:04:31 +0200

    Calculate the moving average parameters for the crossover filter just once

--- src/aax_driver.c
+++ src/aax_driver.c
@@ -369,7 +369,8 @@ aaxDriverOpen(aaxConfig config)
 
             if (handle->info->mode == AAX_MODE_WRITE_SURROUND)
             {
-               float Q = 1.0f, fc = 80.0f;
+               _aaxRingBufferFreqFilterData *freqfilter;
+               float Q = 1.0f, k = 1.0f, fc = 80.0f;
                _filter_t *filter;
 
                /* crossover lowpass filter at 80Hz, 4th orer (24dB/oct) */
@@ -379,6 +380,10 @@ aaxDriverOpen(aaxConfig config)
                filter = aaxFilterSetState(filter, AAX_BESSEL|AAX_24DB_OCT);
                _FILTER_SWAP_SLOT_DATA(handle, SURROUND_CROSSOVER_LP, filter, 0);
                aaxFilterDestroy(filter);
+
+               freqfilter = _FILTER_GET_DATA(handle, SURROUND_CROSSOVER_LP);
+               _aax_movingaverage_fir_compute(fc, freqfilter->fs, &k);
+               freqfilter->k = k;
             }
             else if (handle->info->mode == AAX_MODE_WRITE_HRTF)
             {
