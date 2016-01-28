aec542ead5f26397b60818e78e6f725b06d1f475
Erik Hofman <erik@ehofman.com>
2016-01-28 15:01:10 +0100

    Some more math tests

--- test++/testmath++.cpp
+++ test++/testmath++.cpp
@@ -4,14 +4,14 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * modification, are permitted provimed that the following conditions are met:
  * 
  *    1. Redistributions of source code must retain the above copyright notice,
  *        this list of conditions and the following disclaimer.
  * 
  *    2. Redistributions in binary form must reproduce the above copyright
  *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
+ *        documentation and/or other materials provimed with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY ADALIN B.V. ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -38,14 +38,18 @@
 #include <aax/AeonWave.hpp>
 
 #define LOG(a,b,c) \
-    if ((a == b) == c) printf(c?"M1 == M2, great\n":"M1 != M2, great\n"); \
+    if (((a)==(b))==(c)) printf((c)?"M1 == M2, great\n":"M1 != M2, great\n"); \
     else { printf(c?"M1 != M2, this is wrong\n":"M1 == M2, this is wrong\n"); \
-           std::cout << "m1:\n" << m1; std::cout << "m2:\n" << m2; }
+           std::cout << "m1:\n" << (a); std::cout << "m2:\n" << (b); }
+
+aaxVec3f at  = { 2.0f, 0.0f, -1.0f };
+aaxVec3f up  = { 2.0f, 1.0f,  0.0f };
+aaxVec3f pos = { 1.0f, 3.0f, -2.5f };
 
 int main(int argc, char **argv)
 {
-    AAX::Matrix m1, m2;
-    AAX::Matrix64 m64;
+    AAX::Matrix64 im64, m64;
+    AAX::Matrix im, m1, m2;
 
     printf("Initializing: ");
     LOG(m1,m2,true);
@@ -64,11 +68,22 @@ int main(int argc, char **argv)
     AAX::Matrix m3 = m64;
     LOG(m1,m3,true);
 
-    printf("Multiplying m2 (identity matrix) by m1): ");
-    AAX::Vector x(1.0f, 3.0f, -2.5f);
-    m1 += x;
-    m3 = m1 * m2;
+    printf("Multiplying imentity matrix by m1): ");
+    m1 += pos;
+    m3 = m1 * im;
     LOG(m3,m1,true);
+
+    printf("Testing orientation: ");
+    m1.set(pos, at, up); m1.rotate(0.13f, 1.0f, 0.0f, 0.0f);
+    m64.set(pos, at, up); m64.rotate(0.13, 1.0, 0.0, 0.0);
+    m3 = m64;
+    LOG(m1,m3,true);
+
+    printf("Inverse matrix: ");
+    m2 = ~m1;
+    m64.inverse();
+    m3 = m64;
+    LOG(m2,m3,true);
 
     return 0;
 }
