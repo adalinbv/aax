1b09397c4ca735192a5ec1f5bb2d69c2fef69eb9
Erik Hofman <erik@ehofman.com>
2016-04-03 10:27:01 +0200

    fix offset(aaxType) for AAX::AeonWave and make it possible form devices and interfaces to always return 0

--- src/stream/ext_wav.c
+++ src/stream/ext_wav.c
@@ -1037,7 +1037,7 @@ _aaxFormatDriverUpdateHeader(_driver_t *handle, size_t *bufsize)
       *bufsize = 4*handle->wavBufSize;
       res = handle->wavBuffer;
 
-#if 1
+#if 0
    printf("Write %s Header:\n", extfmt ? "Extnesible" : "Canonical");
    printf(" 0: %08x (ChunkID \"RIFF\")\n", handle->wavBuffer[0]);
    printf(" 1: %08x (ChunkSize: %i)\n", handle->wavBuffer[1], handle->wavBuffer[1]);
