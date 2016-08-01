2101f121115c72b5dbe7bbbab24025d6febef293
Erik Hofman <erik@ehofman.com>
2016-08-01 11:37:20 +0200

    Move the to_string functions to it's own header file and finish it

--- include/aax/aeonwave.hpp
+++ include/aax/aeonwave.hpp
@@ -38,9 +38,11 @@
 
 #include <map>
 #include <vector>
+#include <algorithm>
 
 #include <aax/matrix.hpp>
 #include <aax/aax.h>
+
 
 namespace aax
 {
@@ -782,177 +784,6 @@ private:
     const char* _ed;
     aaxConfig _ec;
 };
-
-inline std::string to_string(enum aaxHandleType handle)
-{
-    switch(handle)
-    {
-    case AAX_CONFIG: return "audio driver";
-    case AAX_BUFFER: return "audio buffer";
-    case AAX_EMITTER: return "sound emitter";
-    case AAX_AUDIOFRAME: return "audio frame";
-    case AAX_FILTER: return "sound filter";
-    case AAX_EFFECT: return "sound effect";
-    case AAX_CONFIG_HD: return "HD audio driver";
-    }
-}
-
-inline std::string to_string(enum aaxFormat fmt)
-{
-    switch(fmt)
-    {
-    case AAX_PCM8S: return "signed, 8-bits per sample";
-    case AAX_PCM16S: return "signed, 16-bits per sample";
-    case AAX_PCM24S: return "signed, 24-bits per sample 32-bit encoded";
-    case AAX_PCM32S: return "signed, 32-bits per sample";
-    case AAX_FLOAT: return "32-bit floating point, -1.0 to 1.0";
-    case AAX_DOUBLE: return "64-bit floating point, -1.0 to 1.0";
-    case AAX_MULAW: return "16-bit compressed to 8-bit µ-law";
-    case AAX_ALAW: return "16-bit compresed to -bit A-law";
-    case AAX_IMA4_ADPCM: return "16-bit compressed to 4-bit addaptive differential";
-    case AAX_PCM8U: return "unsigned, 8-bits per sample";
-    case AAX_PCM16U: return "unsigned, 16-bits per sample";
-    case AAX_PCM24U: return "unsigned, 24-bits per sample 32-bit encoded";
-    case AAX_PCM32U: return "unsigned, 32-bits per sample";
-    case AAX_PCM16S_LE: return "signed, 16-bits per sample little-endian";
-    case AAX_PCM24S_LE: return "signed, 24-bits per sample little-endian 32-bit encoded";
-    case AAX_PCM32S_LE: return "signed, 32-bits per sample little-endian";
-    case AAX_FLOAT_LE: return "32-bit floating point, -1.0 to 1.0 little-endian";
-    case AAX_DOUBLE_LE: return "64-bit floating point, -1.0 to 1.0 little-endian";
-    case AAX_PCM16U_LE: return "unsigned, 16-bits per sample little-endian";
-    case AAX_PCM24U_LE: return "unsigned, 24-bits per sample little-endian 32-bit encoded";
-    case AAX_PCM32U_LE: return "unsigned, 32-bits per sample little-endian";
-    case AAX_PCM16S_BE: return "signed, 16-bits per sample big-endian";
-    case AAX_PCM24S_BE: return "signed, 24-bits per sample big-endian 32-bit encoded";
-    case AAX_PCM32S_BE: return "signed, 32-bits per sample big-endian";
-    case AAX_FLOAT_BE: return "32-bit floating point, -1.0 to 1.0 big-endian";
-    case AAX_DOUBLE_BE: return "64-bit floating point, -1.0 to 1.0 big-endian";
-    case AAX_PCM16U_BE: return "unsigned, 16-bits per sample big-endian";
-    case AAX_PCM24U_BE: return "unsigned, 24-bits per sample big-endian 32-bit encoded";
-    case AAX_PCM32U_BE: return "unsigned, 32-bits per sample big-endian";
-    case AAX_AAXS16S: return "16-bit XML encoded waveform synthesizer";
-    case AAX_AAXS24S: return "24-bit XML encoded waveform synthesizer";
-    }
-}
-
-inline std::string to_string(enum aaxType type)
-{
-    switch(type)
-    {
-    case AAX_LINEAR: return "linear";
-    case AAX_LOGARITHMIC:
-    case AAX_DECIBEL: return "Decibels";
-    case AAX_RADIANS: return "radians";
-    case AAX_DEGREES: return "degrees";
-    case AAX_BYTES: return "bytes";
-    case AAX_FRAMES: return "frames";
-    case AAX_SAMPLES: return "samples";
-    case AAX_MICROSECONDS: return "µ-seconds";
-    }
-}
-
-inline std::string to_string(enum aaxModeType mode)
-{
-    switch(mode)
-    {
-    case AAX_POSITION: return "position";
-    case AAX_LOOPING: return "looping";
-    case AAX_BUFFER_TRACK: return "buffer track";
-    }
-}
-
-inline std::string to_string(enum aaxTrackType type)
-{
-    switch(type)
-    {
-    case AAX_TRACK_MIX: return "mix tracks";
-    case AAX_TRACK_ALL: return "all tracks";
-    case AAX_TRACK0:
-    case AAX_TRACK_LEFT:
-    case AAX_TRACK_FRONT_LEFT: return "front-left";
-    case AAX_TRACK1:
-    case AAX_TRACK_RIGHT:
-    case AAX_TRACK_FRONT_RIGHT: return "front-right";
-    case AAX_TRACK2:
-    case AAX_TRACK_REAR_LEFT: return "rear-left";
-    case AAX_TRACK3:
-    case AAX_TRACK_REAR_RIGHT: return "rear-right";
-    case AAX_TRACK4:
-    case AAX_TRACK_CENTER:
-    case AAX_TRACK_CENTER_FRONT: reutn "center-front";
-    case AAX_TRACK5:
-    case AAX_TRACK_LFE:
-    case AAX_TRACK_SUBWOOFER: "sub-woofer (low-frequency emitter)";
-    case AAX_TRACK6:
-    case AAX_TRACK_SIDE_LEFT: return "side-left";
-    case AAX_TRACK7:
-    case AAX_TRACK_SIDE_RIGHT: return "side-right";
-    }
-}
-
-inline std::string to_string(enum aaxSetupType type)
-{
-    switch(type)
-    {
-    case AAX_NAME_STRING:
-    case AAX_DRIVER_STRING: return "driver name string";
-    case AAX_VERSION_STRING; return "version string";
-    case AAX_RENDERER_STRING: return "renderer string";
-    case AAX_VENDOR_STRING: return "vendor string";
-    case AAX_FREQUENCY: return "frequency";
-    case AAX_TRACKS: return "number of stracks";
-    case AAX_FORMAT: return "audio format";
-    case AAX_REFRESH_RATE:
-    case AAX_REFRESHRATE: return "audio refresh rate";
-    case AAX_TRACK_SIZE:
-    case AAX_TRACKSIZE: return "track size";
-    case AAX_NO_SAMPLES: return "number of samples";
-    case AAX_LOOP_START: return "loop-start point";
-    case AAX_LOOP_END: return "loop-end point";
-    case AAX_MONO_EMITTERS:
-    case AAX_MONO_SOURCES: return "number of mono emitters";
-    case AAX_STEREO_EMITTERS:
-    case AAX_STEREO_SOURCES: return "number of multi-track emitters";
-    case AAX_BLOCK_ALIGNMENT: return "block-alignment";
-    case AAX_AUDIO_FRAMES: return "number of audio frames";
-    case AAX_UPDATE_RATE:
-    case AAX_UPDATERATE: return "4D parameter update rate";
-    case AAX_LATENCY: return "audio latency";
-    case AAX_TRACK_LAYOUT: return "track layout";
-    case AAX_BITRATE: return" bit-rate";
-    case AAX_FRAME_TIMING: return "frame-timing";
-    case AAX_PEAK_VALUE: return "track peak-value";
-    case AAX_AVERAGE_VALUE: return "track average value";
-    case AAX_COMPRESSION_VALUE: return "track compression-value";
-    case AAX_GATE_ENABLED: return "noise-gate enabled";
-    case AAX_SHARED_MODE: return "driver sared-mode";
-    case AAX_TIMER_MODE: return "driver timer-mode";
-    case AAX_BATCHED_MODE: return "driver non-realtime batched-mode";
-    case AAX_SEEKABLE_SUPPORT: return "driver seekable support";
-    case AAX_TRACKS_MIN: return "minimum number of supported playback channels";
-    case AAX_TRACKS_MAX: return "maximum number of supported playback channels";
-    case AAX_PERIODS_MIN: return "minimum number of supported driver periods";
-    case AAX_PERIODS_MAX: return "maximum number of supported driver periods";
-    case AAX_FREQUENCY_MIN: return "minimum supported playback frequency";
-    case AAX_FREQUENCY_MAX: return "maximum supported playback frequency";
-    case AAX_SAMPLES_MAX: return "maximum number of capture samples";
-    case AAX_COVER_IMAGE_DATA: return "album cover art";
-    case AAX_MUSIC_PERFORMER_STRING: return "music performer string";
-    case AAX_MUSIC_GENRE_STRING: return "music genre string";
-    case AAX_TRACK_TITLE_STRING: return "song title string";
-    case AAX_TRACK_NUMBER_STRING: return "song track number string";
-    case AAX_ALBUM_NAME_STRING: return "album name string";
-    case AAX_RELEASE_DATE_STRING: return "song release date string";
-    case AAX_SONG_COPYRIGHT_STRING: return "song copyright string";
-    case AAX_SONG_COMPOSER_STRING: return "song composer string";
-    case AAX_SONG_COMMENT_STRING: return "song comment string";
-    case AAX_ORIGINAL_PERFORMER_STRING: return "song original performer string";
-    case AAX_WEBSITE_STRING: return "artist website string";
-    case AAX_MUSIC_PERFORMER_UPDATE: return "updated music performer string";
-    case AAX_TRACK_TITLE_UPDATE: return "updated song title string";
-    case AAX_RELEASE_MODE: return "release mode";
-    }
-}
 
 } // namespace aax
 
