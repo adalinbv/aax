61102256df323cc97971f5e070c3fc1015c6b54d
Erik Hofman <erik@ehofman.com>
2016-10-17 13:02:41 +0200

    Prepare for using stb_vorbis.c and dr_flac.h; single file, public domain audio decoders

---
+++ src/stream/fmt_flac.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016-2017 by Erik Hofman.
+ * Copyright 2016-2017. by Adalin B.V.
+ * All Rights Reserved.
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Adalin B.V.;
+ * the contents of this file may not be disclosed to third parties, copied or
+ * duplicated in any form, in whole or in part, without the prior written
+ * permission of Adalin B.V.
+ */
+
+#ifndef __FILE_FMT_FLAC_H
+#define __FILE_FMT_FLAC_H 1
+
+#include <aax/aax.h>
+#include <driver.h>
+
+/*
+ * Public domain flac decoder:
+ *
+ * https://github.com/nothings/single_file_libs
+ * https://raw.githubusercontent.com/mackron/dr_libs/master/dr_flac.h
+ */
+
+
+#endif /* __FILE_FMT_FLAC_H */
+
