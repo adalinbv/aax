ec78f675dea5dbd0111fcfcd73a0d788e4c1001d
Erik Hofman <emh@a1.nl>
2017-01-17 00:18:01 +0100

    Add a start of a test utility

---
+++ test/test3dmath.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+// #include <base/types.h>
+// #include <base/geometry.h>
+// #include <src/software/cpu/arch3d_simd.h>
+
+int main(int argc, char *argv[])
+{
+   printf("Hello world\n");
+   return 0;
+}
