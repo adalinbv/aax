1e1d966f68b899df1ad82593442f7764f5f35f67
Erik <erik@ehofman.com>
2017-01-24 16:19:43 +0100

    NEON fixes

--- src/software/cpu/arch3d_neon.c
+++ src/software/cpu/arch3d_neon.c
@@ -20,43 +20,44 @@
 # define vandq_f32(a,b) vreinterpretq_f32_u32(vandq_u32(vreinterpretq_u32_f32(a), vreinterpretq_u32_f32(b)))
 
 static inline float32x4_t
-load_vec3f(const vec3_ptr v)
-{  
+load_vec3f(const vec3f_ptr v)
+{
    static const uint32_t m3a32[] = { 0xffffffff,0xffffffff,0xffffffff,0 };
    return vandq_f32(v->s4, vld1q_f32((const float*)m3a32));
 }
 
 static inline float
 hsum_float32x4_neon(float32x4_t v) {
-    float32x2_ptr r = vadd_f32(vget_high_f32(v), vget_low_f32(v));
+    float32x2_t r = vadd_f32(vget_high_f32(v), vget_low_f32(v));
     return vget_lane_f32(vpadd_f32(r, r), 0);
 }
 
 float
-_vec3fMagnitudeSquared_neon(const vec3_ptr v3)
+_vec3fMagnitudeSquared_neon(const vec3f_ptr v3)
 {
    float32x4_t v = load_vec3f(v3);
    return hsum_float32x4_neon(vmulq_f32(v, v));
 }
 
 float
-_vec3fMagnitude_neon(const vec3_ptr v3)
+_vec3fMagnitude_neon(const vec3f_ptr v3)
 {
    float32x4_t v = load_vec3f(v3);
    return sqrtf(hsum_float32x4_neon(vmulq_f32(v, v)));
 }
 
 float
-_vec3fDotProduct_neon(const vec3_ptr v1, const vec3_ptr v2)
+_vec3fDotProduct_neon(const vec3f_ptr v1, const vec3f_ptr v2)
 {
    return hsum_float32x4_neon(vmulq_f32(load_vec3f(v1), load_vec3f(v2)));
 }
 
 void
-_vec3fCrossProduct_neon(vec3_ptr d, const vec3_ptr v1, const vec3_ptr v2)
+_vec3fCrossProduct_neon(vec3f_ptr d, const vec3f_ptr v1, const vec3f_ptr v2)
 {
-   float32x2x2_t v1lh_2013 = vld2_f32(v1->s4);	// from 0123 to 0213
-   float32x2x2_t v2lh_2013 = vld2_f32(v2->s4);
+   // from 0123 to 0213
+   float32x2x2_t v1lh_2013 = vzip_f32(vget_low_f32(v1->s4), vget_high_f32(v1->s4));
+   float32x2x2_t v2lh_2013 = vzip_f32(vget_low_f32(v2->s4), vget_high_f32(v2->s4));
 
    // from 0213 to 2013
    v1lh_2013.val[0] = vrev64_f32(v1lh_2013.val[0]);
@@ -83,64 +84,52 @@ _vec3fCrossProduct_neon(vec3_ptr d, const vec3_ptr v1, const vec3_ptr v2)
 void
 _vec4fCopy_neon(vec4f_ptr d, const vec4f_ptr v)
 {
-   float32x4_t vec = vld1q_f32(v);
-   vst1q_f32(d, vec);
+   d->s4 = v->s4;
 }
 
 void
 _vec4fMulvec4_neon(vec4f_ptr r, const vec4f_ptr v1, const vec4f_ptr v2)
 {
-   float32x4_t vec1 = vld1q_f32(v1);
-   float32x4_t vec2 = vld1q_f32(v2);
-   vec1 = vmulq_f32(vec1, vec2);
-   vst1q_f32(r, vec1);
+   r->s4 = vmulq_f32(v1->s4, v2->s4);
 }
 
 void
-_vec4fMatrix4_neon(vec4f_ptr d, const vec4f_ptr p, const mtx4_ptr m)
+_vec4fMatrix4_neon(vec4f_ptr d, const vec4f_ptr vi, const mtx4f_ptr m)
 {
-   float32x4_t mx = vld1q_f32((const float *)&m[0]);
-   float32x4_t mv = vmulq_f32(mx, vdupq_n_f32(vi[0]));
    int i;
+
+   d->s4 = vmulq_f32(m->s4x4[0], vdupq_n_f32(vi->v4[0]));
    for (i=1; i<3; ++i) {
-      float32x4_t mx = vld1q_f32((const float *)&m[i]);
-      float32x4_t row = vmulq_f32(mx, vdupq_n_f32(vi[i]));
-      mv = vaddq_f32(mv, row);
+      float32x4_t row = vmulq_f32(m->s4x4[i], vdupq_n_f32(vi->v4[1]));
+      d->s4 = vaddq_f32(d->s4, row);
    }
-   vst1q_f32(d, mv);
 }
 
 void
-_pt4fMatrix4_neon(vec4f_ptr d, const vec4f_ptr vi, const mtx4_ptr m)
+_pt4fMatrix4_neon(vec4f_ptr d, const vec4f_ptr vi, const mtx4f_ptr m)
 {
-   float32x4_t mx = vld1q_f32((const float *)&m[0]);
-   float32x4_t mv = vmulq_f32(mx, vdupq_n_f32(vi[0]));
    int i;
+
+   d->s4 = vmulq_f32(m->s4x4[0], vdupq_n_f32(vi->v4[0]));
    for (i=1; i<3; ++i) {
-      float32x4_t mx = vld1q_f32((const float *)&m[i]);
-      float32x4_t row = vmulq_f32(mx, vdupq_n_f32(vi[i]));
-      mv = vaddq_f32(mv, row);
+      float32x4_t row = vmulq_f32(m->s4x4[i], vdupq_n_f32(vi->v4[1]));
+      d->s4 = vaddq_f32(d->s4, row);
    }
-   mx = vdupq_n_f32((const float *)&m[3]);
-   mv = vaddq_f32(mv, row);
-   vst1q_f32(d, mv);
+   d->s4 = vaddq_f32(d->s4, m->s4x4[3]);
 }
 
 
 void
-_mtx4fMul_neon(mtx4_ptr d, const mtx4_ptr m1, const mtx4_ptr m2)
+_mtx4fMul_neon(mtx4f_ptr d, const mtx4f_ptr m1, const mtx4f_ptr m2)
 {
    int i;
    for (i=0; i<4; ++i) {
-      float32x4_t m1x = vld1q_f32((const float *)&m1[0]);
-      float32x4_t col = vdupq_n_f32(m2[i][0]);
-      float32x4_t row = vmulq_f32(m1x, col);
+      float32x4_t row = vmulq_f32(m1->s4x4[0], vdupq_n_f32(m2->m4[i][0]));
       for (int j=1; j<4; ++j) {
-         m1x = vld1q_f32((const float *)&m1[j]);
-         col = vdupq_n_f32(m2[i][j]);
-         row = vaddq_f32(row, vmulq_f32(m1x, col));
+         float32x4_t col = vdupq_n_f32(m2->m4[i][j]);
+         row = vaddq_f32(row, vmulq_f32(m1->s4x4[j], col));
       }
-      vst1q_f32(d[i], row);
+      d->s4x4[i] = row;
    }
 }
 
