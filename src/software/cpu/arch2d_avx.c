d4f61b6e6d1646a0545f34741333935370b2b72c
Erik Hofman <erik@ehofman.com>
2017-01-31 14:36:18 +0100

    Add all missing sse2 functions to AVX but still as SSE2 code: Switching from AVX to SSE requires a call to _mm256_zeroall() and the remaining code can now be AVX optimized by the compiler

--- src/software/cpu/arch2d_avx.c
+++ src/software/cpu/arch2d_avx.c
@@ -24,6 +24,8 @@
 # define CACHE_ADVANCE_FMADD	 32
 # define CACHE_ADVANCE_CPY	 32
 # define CACHE_ADVANCE_CVT	 64
+# define CACHE_ADVANCE_IMADD     32
+# define CACHE_ADVANCE_INTL      32
 
 
 void
@@ -36,8 +38,8 @@ _batch_cvt24_ps_avx(void_ptr dst, const_void_ptr src, size_t num)
    {
       if (((size_t)d & MEMMASK16) == 0 || ((size_t)s & MEMMASK16) == 0)
       {
-         _mm256_zeroupper();
-         return _batch_cvt24_ps_sse2(dst, src, num);
+         _mm256_zeroall();
+         return _batch_cvt24_ps_avx(dst, src, num);
       }
       else
       {
@@ -344,6 +346,182 @@ _batch_cvtps24_24_avx(void_ptr dst, const_void_ptr src, size_t num)
 }
 
 static void
+_batch_iadd_avx(int32_ptr dst, const_int32_ptr src, size_t num)
+{
+   int32_ptr d = (int32_ptr)dst;
+   int32_ptr s = (int32_ptr)src;
+   size_t i, step, dtmp, stmp;
+
+   dtmp = (size_t)d & MEMMASK;
+   stmp = (size_t)s & MEMMASK;
+   if ((dtmp || stmp) && dtmp != stmp)  /* improperly aligned,            */
+   {                                    /* let the compiler figure it out */
+      i = num;
+      do {
+         *d++ += *s++;
+      } while (--i);
+      return;
+   }
+
+   /* work towards a 16-byte aligned d (and hence 16-byte aligned sptr) */
+   if (dtmp && num)
+   {
+      i = (0x10 - dtmp)/sizeof(int32_t);
+      if (i <= num)
+      {
+         num -= i;
+         do
+         {
+            *d++ += *s++;
+         } while(--i);
+      }
+   }
+
+   step = 2*sizeof(__m128i)/sizeof(int32_t);
+
+   i = num/step;
+   num -= i*step;
+   if (i)
+   {
+      _mm256_zeroall();
+      __m128i *sptr = (__m128i *)s;
+      __m128i *dptr = (__m128i *)d;
+      __m128i xmm0i, xmm3i, xmm4i, xmm7i;
+
+      do
+      {
+         _mm_prefetch(((char *)sptr)+CACHE_ADVANCE_IMADD, _MM_HINT_NTA);;
+         xmm0i = _mm_load_si128(sptr++);
+         xmm4i = _mm_load_si128(sptr++);
+
+         s += step;
+
+         xmm3i = _mm_load_si128(dptr);
+         xmm7i = _mm_load_si128(dptr+1);
+
+         d += step;
+
+         xmm0i = _mm_add_epi32(xmm0i, xmm3i);
+         xmm4i = _mm_add_epi32(xmm4i, xmm7i);
+
+         _mm_store_si128(dptr++, xmm0i);
+         _mm_store_si128(dptr++, xmm4i);
+      }
+      while(--i);
+   }
+
+   if (num)
+   {
+      i = num;
+      do {
+         *d++ += *s++;
+      } while(--i);
+   }
+}
+
+void
+_batch_imadd_avx(int32_ptr dst, const_int32_ptr src, size_t num, float v, float vstep)
+{
+   int32_ptr d = (int32_ptr)dst;
+   int32_ptr s = (int32_ptr)src;
+   size_t i, step, dtmp, stmp;
+
+   if (!num || (v == 0.0f && vstep == 0.0f)) return;
+   if (fabsf(v - 1.0f) < GMATH_128DB && vstep == 0.0f) {
+      _batch_iadd_avx(dst, src, num);
+      return;
+   }
+
+   dtmp = (size_t)d & MEMMASK;
+   stmp = (size_t)s & MEMMASK;
+   if ((dtmp || stmp) && dtmp != stmp)  /* improperly aligned,            */
+   {                                    /* let the compiler figure it out */
+      i = num;
+      do
+      {
+         *d++ += (int32_t)((float)*s++ * v);
+         v += vstep;
+      }
+      while (--i);
+      return;
+   }
+
+   /* work towards a 16-byte aligned d (and hence 16-byte aligned sptr) */
+   if (dtmp && num)
+   {
+      i = (0x10 - dtmp)/sizeof(int32_t);
+      if (i <= num)
+      {
+         num -= i;
+         do
+         {
+            *d++ += (int32_t)((float)*s++ * v);
+            v += vstep;
+         } while(--i);
+      }
+   }
+
+   step = 2*sizeof(__m128i)/sizeof(int32_t);
+
+   vstep *= step;                               /* 8 samples at a time */
+   i = num/step;
+   num -= i*step;
+   if (i)
+   {
+      _mm256_zeroall();
+      __m128i *sptr = (__m128i *)s;
+      __m128i *dptr = (__m128i *)d;
+      __m128 tv = _mm_set1_ps(v);
+      __m128i xmm0i, xmm3i, xmm4i, xmm7i;
+      __m128 xmm1, xmm5;
+
+      do
+      {
+         _mm_prefetch(((char *)sptr)+CACHE_ADVANCE_IMADD, _MM_HINT_NTA);
+         xmm0i = _mm_load_si128(sptr++);
+         xmm4i = _mm_load_si128(sptr++);
+
+         xmm1 = _mm_cvtepi32_ps(xmm0i);
+         xmm5 = _mm_cvtepi32_ps(xmm4i);
+
+         s += step;
+
+         xmm1 = _mm_mul_ps(xmm1, tv);
+         xmm5 = _mm_mul_ps(xmm5, tv);
+
+         xmm0i = _mm_load_si128(dptr);
+         xmm4i = _mm_load_si128(dptr+1);
+
+         xmm3i = _mm_cvtps_epi32(xmm1);
+         xmm7i = _mm_cvtps_epi32(xmm5);
+
+         d += step;
+
+         xmm0i = _mm_add_epi32(xmm0i, xmm3i);
+         xmm4i = _mm_add_epi32(xmm4i, xmm7i);
+
+         v += vstep;
+
+         _mm_store_si128(dptr++, xmm0i);
+         _mm_store_si128(dptr++, xmm4i);
+
+         tv = _mm_set1_ps(v);
+      }
+      while(--i);
+   }
+
+   if (num)
+   {
+      vstep /= step;
+      i = num;
+      do {
+         *d++ += (int32_t)((float)*s++ * v);
+         v += vstep;
+      } while(--i);
+   }
+}
+
+static void
 _batch_fadd_avx(float32_ptr dst, const_float32_ptr src, size_t num)
 {
    float32_ptr s = (float32_ptr)src;
@@ -531,7 +709,294 @@ _batch_fmadd_avx(float32_ptr dst, const_float32_ptr src, size_t num, float v, fl
    }
 }
 
-#if 0
+void
+_batch_cvt24_16_avx(void_ptr dst, const_void_ptr src, size_t num)
+{
+   int16_t *s = (int16_t *)src;
+   int32_t *d = (int32_t*)dst;
+   size_t i, step;
+   size_t tmp;
+
+   if (!num) return;
+
+   /*
+    * work towards 16-byte aligned d
+    */
+   tmp = (size_t)d & MEMMASK;
+   if (tmp && num)
+   {
+      i = (0x10 - tmp)/sizeof(int32_t);
+      if (i <= num)
+      {
+         num -= i;
+         do {
+            *d++ = *s++ << 8;
+         } while(--i);
+      }
+   }
+
+   step = 2*sizeof(__m128i)/sizeof(int16_t);
+
+   tmp = (size_t)s & MEMMASK;
+   i = num/step;
+   num -= i*step;
+   if (i)
+   {
+      _mm256_zeroall();
+      __m128i xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7;
+      __m128i zero = _mm_setzero_si128();
+      __m128i *dptr = (__m128i *)d;
+      __m128i *sptr = (__m128i *)s;
+
+      do {
+         _mm_prefetch(((char *)s)+CACHE_ADVANCE_CVT, _MM_HINT_NTA);
+
+         if (tmp)
+         {
+            xmm0 = _mm_loadu_si128(sptr++);
+            xmm4 = _mm_loadu_si128(sptr++);
+
+            xmm1 = _mm_unpacklo_epi16(zero, xmm0);;
+            xmm3 = _mm_unpackhi_epi16(zero, xmm0);
+            xmm5 = _mm_unpacklo_epi16(zero, xmm4);
+            xmm7 = _mm_unpackhi_epi16(zero, xmm4);
+         }
+         else
+         {
+            xmm0 = _mm_load_si128(sptr++);
+            xmm4 = _mm_load_si128(sptr++);
+
+            xmm1 = _mm_unpacklo_epi16(zero, xmm0);
+            xmm3 = _mm_unpackhi_epi16(zero, xmm0);
+            xmm5 = _mm_unpacklo_epi16(zero, xmm4);
+            xmm7 = _mm_unpackhi_epi16(zero, xmm4);
+         }
+
+         s += step;
+         d += step;
+
+         xmm0 = _mm_srai_epi32(xmm1, 8);
+         xmm2 = _mm_srai_epi32(xmm3, 8);
+         xmm4 = _mm_srai_epi32(xmm5, 8);
+         xmm6 = _mm_srai_epi32(xmm7, 8);
+
+         _mm_store_si128(dptr++, xmm0);
+         _mm_store_si128(dptr++, xmm2);
+         _mm_store_si128(dptr++, xmm4);
+         _mm_store_si128(dptr++, xmm6);
+      }
+      while (--i);
+   }
+
+   if (num)
+   {
+      i = num;
+      do {
+         *d++ = *s++ << 8;
+      } while (--i);
+   }
+}
+
+void
+_batch_cvt16_24_avx(void_ptr dst, const_void_ptr src, size_t num)
+{
+   size_t i, step;
+   int32_t* s = (int32_t*)src;
+   int16_t* d = (int16_t*)dst;
+   size_t tmp;
+
+   /*
+    * work towards 16-byte aligned sptr
+    */
+   tmp = (size_t)s & MEMMASK;
+   if (tmp && num)
+   {
+      i = (0x10 - tmp)/sizeof(int32_t);
+      if (i <= num)
+      {
+         num -= i;
+         do {
+            *d++ = *s++ >> 8;
+         } while(--i);
+      }
+   }
+
+   assert(((size_t)s & MEMMASK) == 0);
+   tmp = (size_t)d & MEMMASK;
+
+   step = 4*sizeof(__m128i)/sizeof(int32_t);
+
+   i = num/step;
+   num -= i*step;
+   if (i)
+   {
+      _mm256_zeroall();
+      __m128i xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7;
+      __m128i *dptr, *sptr;
+
+      dptr = (__m128i *)d;
+      sptr = (__m128i *)s;
+      do
+      {
+         _mm_prefetch(((char *)s)+CACHE_ADVANCE_CVT, _MM_HINT_NTA);
+
+         xmm0 = _mm_load_si128(sptr++);
+         xmm1 = _mm_load_si128(sptr++);
+         xmm4 = _mm_load_si128(sptr++);
+         xmm5 = _mm_load_si128(sptr++);
+
+         xmm2 = _mm_srai_epi32(xmm0, 8);
+         xmm3 = _mm_srai_epi32(xmm1, 8);
+         xmm6 = _mm_srai_epi32(xmm4, 8);
+         xmm7 = _mm_srai_epi32(xmm5, 8);
+
+         s += step;
+
+         xmm4 = _mm_packs_epi32(xmm2, xmm3);
+         xmm6 = _mm_packs_epi32(xmm6, xmm7);
+
+         d += step;
+
+         if (tmp) {
+            _mm_storeu_si128(dptr++, xmm4);
+            _mm_storeu_si128(dptr++, xmm6);
+         } else {
+            _mm_store_si128(dptr++, xmm4);
+            _mm_store_si128(dptr++, xmm6);
+         }
+      }
+      while (--i);
+   }
+
+   if (num)
+   {
+      i = num;
+      do {
+         *d++ = *s++ >> 8;
+      } while (--i);
+   }
+}
+
+void
+_batch_cvt16_intl_24_avx(void_ptr dst, const_int32_ptrptr src,
+                                size_t offset, unsigned int tracks,
+                                size_t num)
+{
+   size_t i, step;
+   int16_t *d = (int16_t*)dst;
+   int32_t *s1, *s2;
+   size_t tmp;
+
+   if (!num) return;
+
+   if (tracks != 2)
+   {
+      size_t t;
+      for (t=0; t<tracks; t++)
+      {
+         int32_t *s = (int32_t *)src[t] + offset;
+         int16_t *d = (int16_t *)dst + t;
+         size_t i = num;
+
+         do
+         {
+            *d = *s++ >> 8;
+            d += tracks;
+         }
+         while (--i);
+      }
+      return;
+   }
+
+   s1 = (int32_t *)src[0] + offset;
+   s2 = (int32_t *)src[1] + offset;
+
+   step = 2*sizeof(__m128i)/sizeof(int32_t);
+
+   /*
+    * work towards 16-byte aligned sptr
+    */
+   tmp = (size_t)s1 & MEMMASK;
+   assert(tmp == ((size_t)s2 & MEMMASK));
+
+   i = num/step;
+   if (tmp && i)
+   {
+      i = (0x10 - tmp)/sizeof(int32_t);
+      num -= i;
+      do
+      {
+         *d++ = *s1++ >> 8;
+         *d++ = *s2++ >> 8;
+      }
+      while (--i);
+   }
+
+   tmp = (size_t)d & MEMMASK;
+   i = num/step;
+   num -= i*step;
+   if (i)
+   {
+      _mm256_zeroall();
+      __m128i mask, xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7;
+      __m128i *dptr, *sptr1, *sptr2;
+
+      sptr1 = (__m128i*)s1;
+      sptr2 = (__m128i*)s2;
+
+      mask = _mm_set_epi32(0x00FFFF00, 0x00FFFF00, 0x00FFFF00, 0x00FFFF00);
+      dptr = (__m128i *)d;
+      do
+      {
+         _mm_prefetch(((char *)s1)+CACHE_ADVANCE_INTL, _MM_HINT_NTA);
+         _mm_prefetch(((char *)s2)+CACHE_ADVANCE_INTL, _MM_HINT_NTA);
+
+         xmm0 = _mm_load_si128(sptr1++);
+         xmm4 = _mm_load_si128(sptr1++);
+         xmm1 = _mm_load_si128(sptr2++);
+         xmm5 = _mm_load_si128(sptr2++);
+
+         xmm2 = _mm_and_si128(xmm0, mask);
+         xmm3 = _mm_and_si128(xmm1, mask);
+         xmm6 = _mm_and_si128(xmm4, mask);
+         xmm7 = _mm_and_si128(xmm5, mask);
+
+         s1 += step;
+         s2 += step;
+
+         xmm0 = _mm_srli_epi32(xmm2, 8);
+         xmm1 = _mm_slli_epi32(xmm3, 8);
+         xmm4 = _mm_srli_epi32(xmm6, 8);
+         xmm5 = _mm_slli_epi32(xmm7, 8);
+
+         xmm0 = _mm_or_si128(xmm1, xmm0);
+         xmm4 = _mm_or_si128(xmm5, xmm4);;
+
+         d += 2*step;
+
+         if (tmp) {
+            _mm_storeu_si128(dptr++, xmm0);
+            _mm_storeu_si128(dptr++, xmm4);
+         } else {
+            _mm_store_si128(dptr++, xmm0);
+            _mm_store_si128(dptr++, xmm4);
+         }
+      } while (--i);
+   }
+
+   if (num)
+   {
+      i = num;
+      do
+      {
+         *d++ = *s1++ >> 8;
+         *d++ = *s2++ >> 8;
+      }
+      while (--i);
+   }
+}
+
+
 void
 _batch_freqfilter_avx(int32_ptr dptr, const_int32_ptr sptr, int t, size_t num, void *flt)
 {
@@ -540,7 +1005,8 @@ _batch_freqfilter_avx(int32_ptr dptr, const_int32_ptr sptr, int t, size_t num, v
 
    if (num)
    {
-      __m256 c, h, mk;
+      _mm256_zeroall();
+      __m128 c, h, mk;
       float *cptr, *hist;
       int stages;
 
@@ -550,9 +1016,9 @@ _batch_freqfilter_avx(int32_ptr dptr, const_int32_ptr sptr, int t, size_t num, v
       if (!stages) stages++;
 
       if (filter->state) {
-         mk = _mm256_set_ss(filter->k * (filter->high_gain - filter->low_gain));
+         mk = _mm_set_ss(filter->k * (filter->high_gain - filter->low_gain));
       } else {
-         mk = _mm256_set_ss(filter->k * filter->high_gain);
+         mk = _mm_set_ss(filter->k * filter->high_gain);
       }
 
       do
@@ -560,50 +1026,50 @@ _batch_freqfilter_avx(int32_ptr dptr, const_int32_ptr sptr, int t, size_t num, v
          int32_ptr d = dptr;
          size_t i = num;
 
-//       c = _mm256_set_ps(cptr[3], cptr[1], cptr[2], cptr[0]);
-         c = _mm256_load_ps(cptr);
-         c = _mm256_shuffle_ps(c, c, _MM_SHUFFLE(3,1,2,0));
+//       c = _mm_set_ps(cptr[3], cptr[1], cptr[2], cptr[0]);
+         c = _mm_load_ps(cptr);
+         c = _mm_shuffle_ps(c, c, _MM_SHUFFLE(3,1,2,0));
 
-//       h = _mm256_set_ps(hist[1], hist[1], hist[0], hist[0]);
-         h = _mm256_loadl_pi(_mm256_setzero_ps(), (__m64*)hist);
-         h = _mm256_shuffle_ps(h, h, _MM_SHUFFLE(1,1,0,0));
+//       h = _mm_set_ps(hist[1], hist[1], hist[0], hist[0]);
+         h = _mm_loadl_pi(_mm_setzero_ps(), (__m64*)hist);
+         h = _mm_shuffle_ps(h, h, _MM_SHUFFLE(1,1,0,0));
 
          do
          {
-            __m256 pz, smp, nsmp, tmp;
+            __m128 pz, smp, nsmp, tmp;
 
-            smp = _mm256_cvtepi32_ps((__m256i)_mm256_load_ss((const float*)s));
+            smp = _mm_cvtepi32_ps((__m128i)_mm_load_ss((const float*)s));
 
             // pz = { c[3]*h1, -c[1]*h1, c[2]*h0, -c[0]*h0 };
-            pz = _mm256_mul_ps(c, h); // poles and zeros
+            pz = _mm_mul_ps(c, h); // poles and zeros
 
-            // smp = *s++ * k;
-            smp = _mm256_mul_ss(smp, mk);
+            // smp = *s++ * k;;
+            smp = _mm_mul_ss(smp, mk);
 
             // tmp[0] = -c[0]*h0 + -c[1]*h1;
-            tmp = _mm256_add_ps(pz, _mm256_shuffle_ps(pz, pz, _MM_SHUFFLE(1,3,0,2)));
+            tmp = _mm_add_ps(pz, _mm_shuffle_ps(pz, pz, _MM_SHUFFLE(1,3,0,2)));
             s++;
 
             // nsmp = smp - h0*c[0] - h1*c[1];
-            nsmp = _mm256_add_ss(smp, tmp);
+            nsmp = _mm_add_ss(smp, tmp);
 
             // h1 = h0, h0 = smp: h = { h0, h0, smp, smp };
-            h = _mm256_shuffle_ps(nsmp, h, _MM_SHUFFLE(0,0,0,0));
+            h = _mm_shuffle_ps(nsmp, h, _MM_SHUFFLE(0,0,0,0));
 
             // tmp[0] = -c[0]*h0 + -c[1]*h1 + c[2]*h0 + c[3]*h1;
-            tmp = _mm256_add_ps(tmp, _mm256_shuffle_ps(tmp, tmp, _MM_SHUFFLE(0,1,2,3)));
+            tmp = _mm_add_ps(tmp, _mm_shuffle_ps(tmp, tmp, _MM_SHUFFLE(0,1,2,3)));
 
             // smp = smp - h0*c[0] - h1*c[1] + h0*c[2] + h1*c[3];
-            smp = _mm256_add_ss(smp, tmp);
-            _mm256_store_ss((float*)d++, (__m256)_mm256_cvtps_epi32(smp));
+            smp = _mm_add_ss(smp, tmp);
+            _mm_store_ss((float*)d++, (__m128)_mm_cvtps_epi32(smp));
          }
          while (--i);
 
-         _mm256_storel_pi((__m64*)hist, h);
+         _mm_storel_pi((__m64*)hist, h);
 
          hist += 2;
          cptr += 4;
-         mk = _mm256_set_ss(1.0f);
+         mk = _mm_set_ss(1.0f);
          s = dptr;
       }
       while (--stages);
@@ -619,9 +1085,10 @@ _batch_freqfilter_float_avx(float32_ptr dptr, const_float32_ptr sptr, int t, siz
 
    if (num)
    {
-      __m256 c, h, mk;
+      _mm256_zeroall();
+      __m128 c, h, mk;
       float *cptr, *hist;
-      int stages;
+      int stages;;
 
       cptr = filter->coeff;
       hist = filter->freqfilter_history[t];
@@ -629,9 +1096,9 @@ _batch_freqfilter_float_avx(float32_ptr dptr, const_float32_ptr sptr, int t, siz
       if (!stages) stages++;
 
       if (filter->state) {
-         mk = _mm256_set_ss(filter->k * (filter->high_gain - filter->low_gain));
+         mk = _mm_set_ss(filter->k * (filter->high_gain - filter->low_gain));
       } else {
-         mk = _mm256_set_ss(filter->k * filter->high_gain);
+         mk = _mm_set_ss(filter->k * filter->high_gain);
       }
 
       do
@@ -639,61 +1106,60 @@ _batch_freqfilter_float_avx(float32_ptr dptr, const_float32_ptr sptr, int t, siz
          float32_ptr d = dptr;
          size_t i = num;
 
-//       c = _mm256_set_ps(cptr[3], cptr[1], cptr[2], cptr[0]);
+//       c = _mm_set_ps(cptr[3], cptr[1], cptr[2], cptr[0]);
          if (((size_t)cptr & MEMMASK) == 0) {
-            c = _mm256_load_ps(cptr);
+            c = _mm_load_ps(cptr);
          } else {
-            c = _mm256_loadu_ps(cptr);
+            c = _mm_loadu_ps(cptr);
          }
-         c = _mm256_shuffle_ps(c, c, _MM_SHUFFLE(3,1,2,0));
+         c = _mm_shuffle_ps(c, c, _MM_SHUFFLE(3,1,2,0));
 
-//       h = _mm256_set_ps(hist[1], hist[1], hist[0], hist[0]);
-         h = _mm256_loadl_pi(_mm256_setzero_ps(), (__m64*)hist);
-         h = _mm256_shuffle_ps(h, h, _MM_SHUFFLE(1,1,0,0));
+//       h = _mm_set_ps(hist[1], hist[1], hist[0], hist[0]);
+         h = _mm_loadl_pi(_mm_setzero_ps(), (__m64*)hist);
+         h = _mm_shuffle_ps(h, h, _MM_SHUFFLE(1,1,0,0));
 
          do
-         {     
-            __m256 pz, smp, nsmp, tmp;
+         {
+            __m128 pz, smp, nsmp, tmp;
 
-            smp = _mm256_load_ss(s);
+            smp = _mm_load_ss(s);
 
             // pz = { c[3]*h1, -c[1]*h1, c[2]*h0, -c[0]*h0 };
-            pz = _mm256_mul_ps(c, h); // poles and zeros
+            pz = _mm_mul_ps(c, h); // poles and zeros
 
             // smp = *s++ * k;
-            smp = _mm256_mul_ss(smp, mk);
+            smp = _mm_mul_ss(smp, mk);
 
             // tmp[0] = -c[0]*h0 + -c[1]*h1;
-            tmp = _mm256_add_ps(pz, _mm256_shuffle_ps(pz, pz, _MM_SHUFFLE(1,3,0,2)));
+            tmp = _mm_add_ps(pz, _mm_shuffle_ps(pz, pz, _MM_SHUFFLE(1,3,0,2)));
             s++;
 
             // nsmp = smp - h0*c[0] - h1*c[1];
-            nsmp = _mm256_add_ss(smp, tmp);
+            nsmp = _mm_add_ss(smp, tmp);
 
             // h1 = h0, h0 = smp: h = { h0, h0, smp, smp };
-            h = _mm256_shuffle_ps(nsmp, h, _MM_SHUFFLE(0,0,0,0));
+            h = _mm_shuffle_ps(nsmp, h, _MM_SHUFFLE(0,0,0,0));
 
             // tmp[0] = -c[0]*h0 + -c[1]*h1 + c[2]*h0 + c[3]*h1;
-            tmp = _mm256_add_ps(tmp, _mm256_shuffle_ps(tmp, tmp, _MM_SHUFFLE(0,1,2,3)));
+            tmp = _mm_add_ps(tmp, _mm_shuffle_ps(tmp, tmp, _MM_SHUFFLE(0,1,2,3)));
 
             // smp = smp - h0*c[0] - h1*c[1] + h0*c[2] + h1*c[3];
-            smp = _mm256_add_ss(smp, tmp);
-            _mm256_store_ss(d++, smp);
+            smp = _mm_add_ss(smp, tmp);
+            _mm_store_ss(d++, smp);
          }
          while (--i);
 
-         h = _mm256_shuffle_ps(h, h, _MM_SHUFFLE(3,1,2,0));
-         _mm256_storel_pi((__m64*)hist, h);
+         h = _mm_shuffle_ps(h, h, _MM_SHUFFLE(3,1,2,0));
+         _mm_storel_pi((__m64*)hist, h);
 
          hist += 2;
          cptr += 4;
-         mk = _mm256_set_ss(1.0f);
+         mk = _mm_set_ss(1.0f);
          s = dptr;
       }
       while (--stages);
    }
 }
-#endif
 
 
 /*
