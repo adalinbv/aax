7df96a993989da258b2f7f6d75f5fde3fd4d2fc2
Erik Hofman <erik@ehofman.com>
2017-07-03 11:49:52 +0200

    First porbe the protocol for song and stream information (OGG contains it) then probe the protocol for it

--- src/stream/device.c
+++ src/stream/device.c
@@ -953,66 +953,106 @@ _aaxStreamDriverGetName(const void *id, int type)
             switch (type)
             {
             case AAX_MUSIC_PERFORMER_STRING:
-               ret = handle->ext->name(handle->ext, __F_ARTIST);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_ARTIST);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_ARTIST);
                }
                break;
             case AAX_MUSIC_PERFORMER_UPDATE:
-               ret = handle->ext->name(handle->ext, __F_ARTIST|__F_NAME_CHANGED);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_ARTIST|__F_NAME_CHANGED);
                }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_ARTIST|__F_NAME_CHANGED);           }
                break;
             case AAX_TRACK_TITLE_STRING:
-               ret = handle->ext->name(handle->ext, __F_TITLE);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_TITLE);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_TITLE);
                }
                break;
             case AAX_TRACK_TITLE_UPDATE:
-               ret = handle->ext->name(handle->ext, __F_TITLE|__F_NAME_CHANGED);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_TITLE|__F_NAME_CHANGED);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_TITLE|__F_NAME_CHANGED);
                }
                break;
             case AAX_MUSIC_GENRE_STRING:
-               ret = handle->ext->name(handle->ext, __F_GENRE);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_GENRE);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_GENRE);
                }
                break;
             case AAX_TRACK_NUMBER_STRING:
-               ret = handle->ext->name(handle->ext, __F_TRACKNO);
+               if (handle->prot) {
+                  ret = handle->prot->name(handle->prot, __F_TRACKNO);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_TRACKNO);
+               }
                break;
             case AAX_ALBUM_NAME_STRING:
-               ret = handle->ext->name(handle->ext, __F_ALBUM);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_ALBUM);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_ALBUM);
                }
                break;
             case AAX_RELEASE_DATE_STRING:
-               ret = handle->ext->name(handle->ext, __F_DATE);
+               if (handle->prot) {
+                  ret = handle->prot->name(handle->prot, __F_DATE);;
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_DATE);
+               }
                break;
             case AAX_SONG_COMPOSER_STRING:
-               ret = handle->ext->name(handle->ext, __F_COMPOSER);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_COMPOSER);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_COMPOSER);
                }
                break;
             case AAX_SONG_COPYRIGHT_STRING:
-               ret = handle->ext->name(handle->ext, __F_COPYRIGHT);
+               if (handle->prot) {
+                  ret = handle->prot->name(handle->prot, __F_COPYRIGHT);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_COPYRIGHT);
+               }
                break;
             case AAX_SONG_COMMENT_STRING:
-               ret = handle->ext->name(handle->ext, __F_COMMENT);
+               if (handle->prot) {
+                  ret = handle->prot->name(handle->prot, __F_COMMENT);
+               }
+               if (!ret) {
+                  ret = handle->ext->name(handle->ext, __F_COMMENT);
+               }
                break;
             case AAX_ORIGINAL_PERFORMER_STRING:
-               ret = handle->ext->name(handle->ext, __F_ORIGINAL);
+               if (handle->prot) {
+                  ret = handle->prot->name(handle->prot, __F_ORIGINAL);
+               }
+               if (!ret) {
+                   ret = handle->ext->name(handle->ext, __F_ORIGINAL);
+               }
                break;
             case AAX_WEBSITE_STRING:
-               ret = handle->ext->name(handle->ext, __F_WEBSITE);
-               if (!ret && handle->prot) {
+               if (handle->prot) {
                   ret = handle->prot->name(handle->prot, __F_WEBSITE);
+               }
+               if (!ret) {
+                   ret = handle->ext->name(handle->ext, __F_WEBSITE);
                }
                break;
             case AAX_COVER_IMAGE_DATA:
