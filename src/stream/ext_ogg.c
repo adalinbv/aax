19e63bb8dc925d93f6f3306b901971f006114236
Erik Hofman <erik@ehofman.com>
2017-08-27 10:40:15 +0200

    32-bit fix

--- src/stream/ext_ogg.c
+++ src/stream/ext_ogg.c
@@ -684,10 +684,10 @@ _getOggPageHeader(_driver_t *handle, uint32_t *header, size_t size)
    if (curr != 0x5367674f)		/* OggS */
    {
       char *c = strncasestr((const char*)header, "OggS", size);
-      if (!c) printf("OggS header not found, len: %lu\n", size);
+      if (!c) printf("OggS header not found, len: %zu\n", size);
       else
       {
-         printf("Found OggS header at offset: %li\n", c-(char*)header);
+         printf("Found OggS header at offset: %zi\n", c-(char*)header);
          header = (uint32_t*)c;
          curr = header[0];
       }
