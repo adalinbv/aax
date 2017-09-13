378290c6c20ffbaa576bd2d313cf39f5b753e04d
Erik Hofman <erik@ehofman.com>
2017-09-13 13:10:03 +0200

    Modify or add the copyright headers

--- base/geometry.c
+++ base/geometry.c
@@ -1,11 +1,24 @@
 /*
- * geometry.c : geometry related functions.
+ * Copyright 2005-2017 by Erik Hofman.
+ * Copyright 2009-2017 by Adalin B.V.
  *
- * Copyright (C) 2005-2017 by Erik Hofman <erik@ehofman.com>
+ * This file is part of AeonWave
  *
- * $Id: Exp $
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
+
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif
