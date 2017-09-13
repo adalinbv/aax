a025584e8885d0770a99c1f57cb16cac674bdcc5
Erik Hofman <erik@ehofman.com>
2017-09-13 13:10:03 +0200

    Modify or add the copyright headers

--- base/buffers.c
+++ base/buffers.c
@@ -1,21 +1,23 @@
 /*
  * Copyright (C) 2005-2017 by Erik Hofman.
- * Copyright (C) 2007-2017 by Adalin B.V.
+ * Copyright 2007-2017 by Erik Hofman.
+ * Copyright 2009-2017 by Adalin B.V.
  *
- * This file is part of AeonWave-OpenAL.
+ * This file is part of AeonWave
  *
- *  AeonWave-OpenAL is free software: you can redistribute it and/or modify
- *  it under the terms of the Lesser GNU General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  AeonWave-OpenAL is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the Lesser GNU General Public License
- *  along with AeonWave-OpenAL.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #if HAVE_CONFIG_H
--- base/geometry.h
+++ base/geometry.h
@@ -1,25 +1,22 @@
 /*
- * geometry.h : math related function definitions.
+ * Copyright 2005-2017 by Erik Hofman.
+ * Copyright 2009-2017 by Adalin B.V.
  *
- * Copyright (C) 2005-2017 by Erik Hofman <erik@ehofman.com>
+ * This file is part of AeonWave
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA  02111-1307, USA.
- *
- * $Id: Exp $
- *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #ifndef _GEOMETRY_H
