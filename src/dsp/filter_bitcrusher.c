ef66b02d1073320386140ffff76ca0fc48ec1e3c
Erik Hofman <erik@ehofman.com>
2017-11-06 13:25:03 +0100

    Add a bitcrisher filter

---
+++ src/dsp/filter_bitcrusher.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2007-2017 by Erik Hofman.
+ * Copyright 2009-2017 by Adalin B.V.
+ *
+ * This file is part of AeonWave
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h>
+#ifdef HAVE_RMALLOC_H
+# include <rmalloc.h>
+#else
+# include <stdlib.h>
+# include <malloc.h>
+#endif
+
+#include <aax/aax.h>
+
+#include <base/types.h>		/* for rintf */
+#include <base/gmath.h>
+
+#include "common.h"
+#include "filters.h"
+#include "api.h"
+
+static aaxFilter
+_aaxBitCrusherFilterCreate(_aaxMixerInfo *info, enum aaxFilterType type)
+{
+   _filter_t* flt = _aaxFilterCreateHandle(info, type, 1);
+   aaxFilter rv = NULL;
+
+   if (flt)
+   {
+      _aaxSetDefaultFilter2d(flt->slot[0], flt->pos);
+      rv = (aaxFilter)flt;
+   }
+   return rv;
+}
+
+static int
+_aaxBitCrusherFilterDestroy(_filter_t* filter)
+{
+   free(filter);
+
+   return AAX_TRUE;
+}
+
+static aaxFilter
+_aaxBitCrusherFilterSetState(_filter_t* filter, int state)
+{
+   filter->slot[0]->state = state;
+   return filter;
+}
+
+static _filter_t*
+_aaxNewBitCrusherFilterHandle(const aaxConfig config, enum aaxFilterType type, _aax2dProps* p2d, UNUSED(_aax3dProps* p3d))
+{
+   _handle_t *handle = get_driver_handle(config);
+   _aaxMixerInfo* info = handle ? handle->info : _info;
+   _filter_t* rv = _aaxFilterCreateHandle(info, type, 1);
+
+   if (rv)
+   {
+      unsigned int size = sizeof(_aaxFilterInfo);
+
+      memcpy(rv->slot[0], &p2d->filter[rv->pos], size);
+      rv->slot[0]->data = NULL;
+
+      rv->state = p2d->filter[rv->pos].state;
+   }
+   return rv;
+}
+
+static float
+_aaxBitCrusherFilterSet(float val, int ptype, UNUSED(unsigned char param))
+{
+   float rv = val;
+   if (ptype == AAX_LOGARITHMIC) {
+      rv = _lin2db(val);
+   }
+   return rv;
+}
+
+static float
+_aaxBitCrusherFilterGet(float val, int ptype, UNUSED(unsigned char param))
+{
+   float rv = val;
+   if (param < 3 && ptype == AAX_LOGARITHMIC) {
+      rv = _db2lin(val);
+   }
+   return rv;
+}
+
+static float
+_aaxBitCrusherFilterMinMax(float val, int slot, unsigned char param)
+{
+  static const _flt_minmax_tbl_t _aaxBitCrusherRange[_MAX_FE_SLOTS] =
+   {    /* min[4] */                  /* max[4] */
+    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 1.0f } },
+    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } }, 
+    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } },
+    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } }
+   };
+   
+   assert(slot < _MAX_FE_SLOTS);
+   assert(param < 4);
+   
+   return _MINMAX(val, _aaxBitCrusherRange[slot].min[param],
+                       _aaxBitCrusherRange[slot].max[param]);
+}
+
+/* -------------------------------------------------------------------------- */
+
+_flt_function_tbl _aaxBitCrusherFilter =
+{
+   AAX_TRUE,
+   "AAX_bitcrusher_filter", 1.0f,
+   (_aaxFilterCreate*)&_aaxBitCrusherFilterCreate,
+   (_aaxFilterDestroy*)&_aaxBitCrusherFilterDestroy,
+   (_aaxFilterSetState*)&_aaxBitCrusherFilterSetState,
+   (_aaxNewFilterHandle*)&_aaxNewBitCrusherFilterHandle,
+   (_aaxFilterConvert*)&_aaxBitCrusherFilterSet,
+   (_aaxFilterConvert*)&_aaxBitCrusherFilterGet,
+   (_aaxFilterConvert*)&_aaxBitCrusherFilterMinMax
+};
+
