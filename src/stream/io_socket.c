1d90a9bfa0412bcb516c97e8e171d09223d826aa
Erik <emh@a1.nl>
2018-01-13 22:50:36 +0100

    Fix the timeout the proper way

--- src/stream/io_socket.c
+++ src/stream/io_socket.c
@@ -107,12 +107,15 @@ _socket_open(_io_t *io, const char *server)
                setsockopt(fd, SOL_SOCKET, SO_NOSIGPIPE, &on, sizeof(on));
 #endif
                setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, (char*)&on, sizeof(on));
-//             setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(tv));
+               setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(tv));
                setsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char*)&size,sizeof(size));
-               io->error_max = (unsigned)(500.0f/timeout_ms); // 0.1 sec.
+               io->error_max = (unsigned)(3000.0f/timeout_ms); // 3.0 sec.
 #if 0
  unsigned int m;
  int n;
+
+ printf("timeout_ms: %i\n", timeout_ms);
+ printf("error_max: %i\n", io->error_max);
 
  m = sizeof(n);
  getsockopt(fd, SOL_SOCKET, SO_RCVBUF, &n, &m);
