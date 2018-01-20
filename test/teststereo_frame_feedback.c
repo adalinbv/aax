61825836dbac15accfea80199357b13e379e8c34
Erik Hofman <erik@ehofman.com>
2018-01-20 14:38:31 +0100

    Add a test utility to test audio frame feedback (registering two frames to eachother)

---
+++ test/teststereo_frame_feedback.c
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2008-2018 by Erik Hofman.
+ * Copyright (C) 2009-2018 by Adalin B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *        this list of conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY ADALIN B.V. ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ * NO EVENT SHALL ADALIN B.V. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUTOF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Adalin B.V.
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+
+#include <aax/aax.h>
+
+#include "base/types.h"
+#include "driver.h"
+#include "wavfile.h"
+
+#define FILE_PATH		SRC_PATH"/stereo.wav"
+
+/*
+ * 1. register frame2 to the mixer
+ * 2. register frame2 to frame1
+ * 3. register frame1 to frame2
+ * 4. register emitter to frame2 
+ */
+
+int main(int argc, char **argv)
+{
+    char *devname, *infile;
+    aaxConfig config;
+    int res, rv = 0;
+
+    devname = getDeviceName(argc, argv);
+    infile = getInputFile(argc, argv, FILE_PATH);
+
+    config = aaxDriverOpenByName(devname, AAX_MODE_WRITE_STEREO);
+    testForError(config, "No default audio device available.");
+
+    if (config && (rv >= 0))
+    {
+        aaxBuffer buffer = bufferFromFile(config, infile);
+        if (buffer)
+        {
+            aaxFrame frame, frame2;
+            aaxEmitter emitter;
+            aaxEffect effect;
+            float pitch, dt = 0.0f;
+            int q, state;
+
+            /** mixer */
+            res = aaxMixerSetState(config, AAX_INITIALIZED);
+            testForState(res, "aaxMixerInit");
+
+            res = aaxMixerSetState(config, AAX_PLAYING);
+            testForState(res, "aaxMixerStart");
+
+            /** audio frame 1 */
+            frame = aaxAudioFrameCreate(config);
+            testForError(frame, "Unable to create a new audio frame\n");
+
+            /** schedule the audioframe for playback */
+            res = aaxAudioFrameSetState(frame, AAX_PLAYING);
+            testForState(res, "aaxAudioFrameStart");
+
+            /** audio frame 2 */
+            frame2 = aaxAudioFrameCreate(config);
+            testForError(frame2, "Unable to create a new audio frame 2\n");
+
+            /** register audio frame 2 to the mixer*/
+            res = aaxMixerRegisterAudioFrame(config, frame2);
+            testForState(res, "aaxMixerRegisterAudioFrame");
+
+            /** register audio frame 2 to audio frame 1*/
+            res = aaxAudioFrameRegisterAudioFrame(frame, frame2);
+            testForState(res, "aaxAudioFrameRegisterAudioFrame frame2 to frame1");
+
+            /** schedule the audioframe for playback */
+            res = aaxAudioFrameSetState(frame2, AAX_PLAYING);
+            testForState(res, "aaxAudioFrameStart");
+
+            /** register audio frame 1  to audio frame 2*/
+            res = aaxAudioFrameRegisterAudioFrame(frame2, frame);
+            testForState(res, "aaxAudioFrameRegisterAudioFrame frame1 to frame2");
+
+            /** emitter */
+            emitter = aaxEmitterCreate();
+            testForError(emitter, "Unable to create a new emitter");
+
+            res = aaxEmitterAddBuffer(emitter, buffer);
+            testForState(res, "aaxEmitterAddBuffer");
+
+            /* pitch */
+            pitch = getPitch(argc, argv);
+            effect = aaxEffectCreate(config, AAX_PITCH_EFFECT);
+            testForError(effect, "Unable to create the pitch effect");
+
+            res = aaxEffectSetParam(effect, AAX_PITCH, AAX_LINEAR, pitch);
+            testForState(res, "aaxEffectSetParam");
+
+            res = aaxEmitterSetEffect(emitter, effect);
+            testForState(res, "aaxEmitterSetPitch");
+            aaxEffectDestroy(effect);
+
+            /** register emitter */
+            res = aaxAudioFrameRegisterEmitter(frame2, emitter);
+            testForState(res, "aaxMixerRegisterEmitter");
+
+            /** schedule the emitter for playback */
+            res = aaxEmitterSetState(emitter, AAX_PLAYING);
+            testForState(res, "aaxEmitterStart");
+
+            q = 0;
+            do
+            {
+                msecSleep(50);
+                dt += 0.05f;
+
+                if (++q > 10)
+                {
+                    unsigned long offs, offs_bytes;
+                    float off_s;
+                    q = 0;
+
+                    off_s = aaxEmitterGetOffsetSec(emitter);
+                    offs = aaxEmitterGetOffset(emitter, AAX_SAMPLES);
+                    offs_bytes = aaxEmitterGetOffset(emitter, AAX_BYTES);
+                    printf("playing time: %5.2f, buffer position: %5.2f "
+                           "(%li samples/ %li bytes)\n", dt, off_s,
+                           offs, offs_bytes);
+                }
+                state = aaxEmitterGetState(emitter);
+            }
+            while (state == AAX_PLAYING);
+
+            res = aaxAudioFrameDeregisterEmitter(frame, emitter);
+            res = aaxAudioFrameSetState(frame, AAX_STOPPED);
+            res = aaxMixerDeregisterAudioFrame(config, frame);
+            res = aaxMixerSetState(config, AAX_STOPPED);
+            res = aaxEmitterDestroy(emitter);
+            res = aaxBufferDestroy(buffer);
+            res = aaxAudioFrameDestroy(frame);
+        }
+    }
+
+    res = aaxDriverClose(config);
+    res = aaxDriverDestroy(config);
+
+    return rv;
+}
