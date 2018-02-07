d0c57faffafc6fe228c86be26488549b20284fae
Erik Hofman <erik@ehofman.com>
2018-02-07 14:15:06 +0100

    Add double precision versions of vec3dAbsolute and vec3dLessThan

--- base/geometry.h
+++ base/geometry.h
@@ -156,12 +156,14 @@ typedef void (*vec3dCopy_proc)(vec3d_ptr d, const vec3d_ptr v);
 typedef void (*vec3fMulvec3f_proc)(vec3f_ptr r, const vec3f_ptr v1, const vec3f_ptr v2);
 typedef void (*vec3dMulvec3d_proc)(vec3d_ptr r, const vec3d_ptr v1, const vec3d_ptr v2);
 typedef void (*vec3fAbsolute_proc)(vec3f_ptr d, const vec3f_ptr v);
+typedef void (*vec3dAbsolute_proc)(vec3d_ptr d, const vec3d_ptr v);
 
 extern vec3fCopy_proc vec3fCopy;
 extern vec3dCopy_proc vec3dCopy;
 extern vec3fMulvec3f_proc vec3fMulvec3;
 extern vec3dMulvec3d_proc vec3dMulvec3;
 extern vec3fAbsolute_proc vec3fAbsolute;
+extern vec3dAbsolute_proc vec3dAbsolute;
 
 void vec3dFill(double d[3], double v[3]);
 void vec3dFillf(double d[3], float v[3]);
@@ -169,6 +171,7 @@ void vec3dAdd(vec3d_ptr d, const vec3d_ptr v1, const vec3d_ptr v2);
 void vec3dSub(vec3d_ptr d, const vec3d_ptr v1, const vec3d_ptr v2);
 void vec3dScalarMul(vec3d_ptr d, const vec3d_ptr r, float v);
 void vec3dNegate(vec3d_ptr d, const vec3d_ptr v);
+int vec3dLessThan(const vec3d_ptr v1, const vec3d_ptr v);
 
 void vec3fFill(float d[3], float v[3]);
 void vec3fFilld(float d[3], double v[3]);
@@ -259,6 +262,7 @@ float _vec3fNormalize_cpu(vec3f_ptr d, const vec3f_ptr v);
 double _vec3dNormalize_cpu(vec3d_ptr d, const vec3d_ptr v);
 void _vec3fCrossProduct_cpu(vec3f_ptr d, const vec3f_ptr v1, const vec3f_ptr v2);
 void _vec3fAbsolute_cpu(vec3f_ptr d, const vec3f_ptr v);
+void _vec3dAbsolute_cpu(vec3d_ptr d, const vec3d_ptr v);
 
 void _vec4fCopy_cpu(vec4f_ptr d, const vec4f_ptr v);
 void _mtx4fCopy_cpu(mtx4f_ptr d, const mtx4f_ptr m);
