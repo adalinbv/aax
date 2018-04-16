d6ae321cc513335c9f371ee1c6e41fa1909495b5
Erik Hofman <erik@ehofman.com>
2018-04-16 11:26:14 +0200

    c could be 0.0f which leads to a nan in the lorentz function

--- src/dsp/effect_velocity.c
+++ src/dsp/effect_velocity.c
@@ -173,8 +173,8 @@ _velocity_prepare(_aax3dProps *ep3d, _aaxDelayed3dProps *edp3d, _aaxDelayed3dPro
    if (dist_ef > 1.0f)
    {
       _aaxPitchShiftFn* dopplerfn;
-      FLOAT c, ve2;
       float ve;
+      FLOAT c;
 
       *(void**)(&dopplerfn) = _EFFECT_GET_DATA(ep3d, VELOCITY_EFFECT);
       assert(dopplerfn);
@@ -184,9 +184,13 @@ _velocity_prepare(_aax3dProps *ep3d, _aaxDelayed3dProps *edp3d, _aaxDelayed3dPro
        */
       mtx4fMul(&edp3d_m->velocity, &fdp3d_m->velocity, &edp3d->velocity);
 
-      ve2 = vec3fMagnitudeSquared(&edp3d_m->velocity.v34[LOCATION]);
+      df = 0.0f;
       c = _EFFECT_GET(ep3d, VELOCITY_EFFECT, AAX_LIGHT_VELOCITY);
-      df = _lorentz(ve2, c*c);
+      if (c > 0.0f)
+      {
+         FLOAT ve2 = vec3fMagnitudeSquared(&edp3d_m->velocity.v34[LOCATION]);
+         df = _lorentz(ve2, c*c);
+      }
 
       ve = vec3fDotProduct(&edp3d_m->velocity.v34[LOCATION], epos);
       df = _MAX(df + dopplerfn(ve, vs/sdf), 0.1f); // prevent negative pitch
