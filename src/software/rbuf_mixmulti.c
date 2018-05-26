797b1ab9ceada8489e3a006e4daaff332b11a067
Erik Hofman <erik@ehofman.com>
2018-05-26 11:10:56 +0200

    Take the square of all LFO related gains for a more natural sounding attack and decay

--- src/software/rbuf_mixmulti.c
+++ src/software/rbuf_mixmulti.c
@@ -183,7 +183,7 @@ _aaxRingBufferMixMulti16(_aaxRingBuffer *drb, _aaxRingBuffer *srb, const _aaxMix
    }
 
    /* final emitter volume */
-   gain *= _FILTER_GET(ep2d, VOLUME_FILTER, AAX_GAIN);
+   gain = _square(gain)*_FILTER_GET(ep2d, VOLUME_FILTER, AAX_GAIN);
    if (genv) genv->value_total = gain;
 
    /** Automatic volume ramping to avoid clicking */
