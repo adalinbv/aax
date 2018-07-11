d12df14c415879b4f4ccec68eaf660adb775545e
Erik Hofman <erik@ehofman.com>
2018-07-11 07:50:50 +0200

    Update the copyright date

--- include/aax/strings.hpp
+++ include/aax/strings.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2017 by Erik Hofman.
- * Copyright (C) 2015-2017 by Adalin B.V.
+ * Copyright (C) 2015-2018 by Erik Hofman.
+ * Copyright (C) 2015-2018 by Adalin B.V.
  *
  * This file is part of AeonWave
  *
