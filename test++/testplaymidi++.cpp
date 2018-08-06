9107e4e842e51ac42d3dc8696ce91f6ba4daebfc
Erik Hofman <erik@ehofman.com>
2018-08-06 22:33:22 +0200

    Commit the current state

--- test++/testplaymidi++.cpp
+++ test++/testplaymidi++.cpp
@@ -28,7 +28,8 @@
 #include "config.h"
 #endif
 
-#include <stdio.h>		/* for NULL */
+#include <stdio.h>
+#include <assert.h>
 #ifdef HAVE_RMALLOC_H
 # include <rmalloc.h>
 #else
@@ -43,6 +44,7 @@
 #include <aax/aeonwave.hpp>
 #include <aax/instrument.hpp>
 
+#include "buffer_map.hpp"
 #include "driver.h"
 
 #ifndef _countof
@@ -58,11 +60,127 @@
 # define LOG			printf
 #endif
 
+typedef buffer_map<unsigned char> MidiBuffer;
+int verbose = 0;
+
 void printNote(unsigned char note)
 {
     const char *notes[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
     LOG("%s%i", notes[note % 12], (note / 12)-1);
 }
+
+class Piano
+{
+    struct Channel
+    {
+        float time;
+        uint32_t velocity;
+        unsigned char note;
+    };
+
+    Channel m_channel[20];
+    float m_noteDuration;
+
+public:
+    Piano()
+    {
+        m_noteDuration = 0.3f; // in seconds
+
+        for (int i = 0; i < _countof(m_channel); i++)
+            memset(&m_channel[i], 0, sizeof(Channel));
+    }
+
+    int stop(float t, char channelId, unsigned char note, unsigned char velocity)
+    {
+        if (channelId == -1)
+        {
+            // find the requested note
+            for (int i = 0; i < _countof(m_channel); i++)
+            {
+                if (m_channel[i].note == note)
+                {
+                    channelId = i;
+                    break;
+                }
+            }
+        }
+
+        if (channelId >=0) // stop playback
+        {
+            m_channel[channelId].note = 0;
+            m_channel[channelId].velocity = velocity;
+            m_channel[channelId].time = t;
+        }
+
+        return channelId;
+    }
+
+    int start(float t, char channelId, unsigned char note, unsigned char velocity)
+    {
+        if (channelId == -1)
+        {
+            // find an empty note
+            for (int i = 0; i < _countof(m_channel); i++)
+            {
+                if (m_channel[i].note == 0)
+                {
+                    channelId = i;
+                    break;
+                }
+            }
+        }
+
+        if (channelId >= 0)     // start playback
+        {
+            m_channel[channelId].note = note;
+            m_channel[channelId].velocity = velocity;
+            m_channel[channelId].time = t;
+        }
+
+        return channelId;
+    }
+
+    float synthesize(float time)
+    {
+        float val = 0;
+
+        for (int i = 0; i < _countof(m_channel); i++)
+        {
+            if (m_channel[i].note > 0)
+            {
+                float vol = ((m_channel[i].time + m_noteDuration) - time) / m_noteDuration;
+
+                if (vol > 0)
+                    val += renderNote(time, NoteToFreq(m_channel[i].note), vol);
+                else
+                    m_channel[i].note = 0;
+            }
+        }
+
+        val /= _countof(m_channel);
+
+        return val;
+    }
+
+private:
+    float NoteToFreq(int note) {
+        return (float)pow(2.0f, (note - 49) / 12.0f) * 440.0f;
+    }
+
+    float renderNote(float time, float freq, float vol)
+    {
+        float v = (float)(cosf(2.0f * (float)M_PI * time * freq));
+        if (v > 0) {
+            v = vol;
+        } else if (v < 0) {
+            v = -vol;
+        }
+        return v;
+    }
+};
+
+Piano piano;
+
 
 ///////////////////////////////////////////////////////////////
 // MidiStream 
@@ -70,48 +188,38 @@ void printNote(unsigned char note)
 class MidiStream
 {
 public:
-    unsigned char *m_pTrack = NULL;
-    unsigned char *m_pTrackFin = NULL;
-
-    MidiStream(unsigned char* ini, size_t& length)
-    {
-        m_pTrack = ini;
-        m_pTrackFin = ini+length;
-    }
+    MidiStream(MidiBuffer& track) : track(track), pos(0) {}
 
     uint32_t GetVar()
     {
-        uint32_t out = 0;
+        uint32_t rv = 0;
 
-        for (;;)
+        while(pos < track.size())
         {
-            out <<= 7;
-            out += *m_pTrack & 0x7f;
-
-            if ((*m_pTrack & 0x80) == 0x00)
+            rv <<= 7;
+            rv += track[pos] & 0x7f;
+            if ((track[pos] & 0x80) == 0x00) {
                 break;
-
-            m_pTrack++;
+            }
+            pos++;
         }
+        pos++;
 
-        m_pTrack++;
-
-        return out;
+        return rv;
     }
 
-    void Back()
+    inline void Back()
     {
-        m_pTrack--;
+        assert(pos > 0);
+        pos--;
     }
 
-    unsigned char GetByte()
-    {
-        return *m_pTrack++;
+    inline unsigned char GetByte() {
+        return track[pos++];
     }
 
-    void Skip(int count)
-    {
-        m_pTrack+= count;
+    inline void Skip(int count) {
+        pos += count;
     }
 
     uint32_t GetLength(int bytes)
@@ -123,46 +231,31 @@ public:
         return val;
     }
 
-    bool done()
-    {
-        int left = m_pTrackFin - m_pTrack;
-        return left <= 0;
+    inline bool done() {
+        return (pos == track.size());
     }
+
+public:
+    MidiBuffer track;
+    size_t pos;
 };
 
 ///////////////////////////////////////////////////////////////
 // MidiTrack
 ///////////////////////////////////////////////////////////////
 
-uint32_t ticksPerQuarterNote=24;
-float quarterNote = 24;
-uint32_t bpm = 120;
-
 class MidiTrack : public MidiStream
 {
-    bool m_omni;
-    float m_nextTime;
-    uint32_t m_channel;
-    unsigned char m_lastType;
 public:
-
-    MidiTrack(uint32_t channel, unsigned char *ini, size_t length)
-        : MidiStream(ini, length)
-    {
-        m_nextTime = (float)GetVar();
-        m_channel = channel;
-        m_omni = false;
-    }
+    MidiTrack(uint32_t channel, MidiBuffer& track, uint32_t ppq)
+        : MidiStream(track), m_nextTime(GetVar()),
+          m_channel(channel), PPQ(ppq), m_omni(false) {}
 
     void play(float seconds)
     {
-        while (m_nextTime <= seconds)
+printf("m_nextTime: %f (%f)\n", m_nextTime, seconds);
+        if (!done() && m_nextTime <= seconds)
         {
-            if (done() == true)
-            {
-                break;
-            }
-
             unsigned char type = GetByte();
             
             if ((type & 0x80) == 0)
@@ -184,26 +277,29 @@ public:
                 unsigned char metaType = GetByte();
                 unsigned char length = GetByte();
 
-                LOG("    meta: 0x%02x", metaType);
-                LOG("    length: 0x%02x", length);
+                if (verbose)
+                {
+                   LOG("    meta: 0x%02x", metaType);
+                   LOG("    length: 0x%02x", length);
 
-                if (metaType == 0x01)
-                {
-                    LOG("    text event: ");
-                    for (int i = 0; i<length; i++)
-                        LOG("%c", GetByte());
-                }
-                else if (metaType == 0x03)
-                {
-                    LOG("    track name: ");
-                    for (int i = 0; i<length; i++)
-                        LOG("%c", GetByte());
-                }
-                else if (metaType == 0x09)
-                {
-                    LOG("    inst: ");
-                    for (int i = 0; i<length; i++)
-                        LOG("%c", GetByte());
+                   if (metaType == 0x01)
+                   {
+                       LOG("    text event: ");
+                       for (int i = 0; i<length; i++)
+                           LOG("%c", GetByte());
+                   }
+                   else if (metaType == 0x03)
+                   {
+                       LOG("    track name: ");
+                       for (int i = 0; i<length; i++)
+                           LOG("%c", GetByte());
+                   }
+                   else if (metaType == 0x09)
+                   {
+                       LOG("    inst: ");
+                       for (int i = 0; i<length; i++)
+                           LOG("%c", GetByte());
+                   }
                 }
                 else if (metaType == 0x20)
                 {
@@ -245,6 +341,7 @@ public:
                 {
                     unsigned char nn = GetByte();
                     LOG("************ THE END ************\n");
+                    return;
                 }
                 else
                 {
@@ -271,16 +368,12 @@ public:
                 {                    
                     unsigned char key = GetByte();
                     unsigned char speed = GetByte();
-
                     int i;
 
-                    if (subType == 8)
-                    {
-                        i = instr.release(seconds, m_omni ? channel : -1, key, speed);
-                    }
-                    else if (subType == 9)
-                    {
-                        i = instr.push(seconds, m_omni ? channel : -1, key, speed);
+                    if (subType == 8) {
+                        i = piano.stop(seconds, m_omni ? channel : -1, key, speed);
+                    } else if (subType == 9) {
+                        i = piano.start(seconds, m_omni ? channel : -1, key, speed);
                     }
 
                     //'LOG("    ac: %c ch: %i note: ", channel + 0*(subType == 8) ?'^':'v', i);
@@ -322,23 +415,34 @@ public:
                     unsigned char lsb = GetByte();
                     unsigned char msb = GetByte();
                 }
-                else {
+                else if (verbose) {
                     LOG("    unknown type: 0x%02x", type);
                 }
             }            
 
             LOG("\n");
 
-            if (done())
-                break;
+            if (!done())
+            {
+                float c = (float)(bpm * PPQ) / 60.0f;
 
-            float c = (float)(bpm * ticksPerQuarterNote) / 60.0f;
-
-            uint32_t deltaTicks = GetVar();
-            if (c >0)
-                m_nextTime += deltaTicks / c;
+                uint32_t deltaTicks = GetVar();
+                if (c >0) {
+                    m_nextTime += deltaTicks / c;
+                }
+            }
         }
     }
+
+private:
+    float m_nextTime;
+    uint32_t m_channel;
+    uint32_t PPQ;
+    unsigned char m_lastType;
+    bool m_omni;
+
+    float quarterNote = 24;
+    uint32_t bpm = 120;
 };
 
 ///////////////////////////////////////////////////////////////
@@ -348,7 +452,7 @@ public:
 class Midi
 {
 public:
-    Midi() : tracks(0), time(0.0f) {}
+    Midi() : no_tracks(0), time(0.0f) {}
 
     Midi(const char *filename) : Midi()
     {
@@ -360,7 +464,8 @@ public:
         midi.resize(size);
         if (midi.size() && file.read((char*)midi.data(), fileSize))
         {
-            MidiStream ch(midi.data(), size);
+            MidiBuffer map(midi.data(), size);
+            MidiStream ch(map);
 
             if (ch.GetByte() == 'M' && ch.GetByte() == 'T' &&
                 ch.GetByte() == 'h' && ch.GetByte() == 'd')
@@ -368,21 +473,21 @@ public:
                 ch.GetLength(4);
 
                 uint32_t format = (ch.GetByte() << 8) + ch.GetByte();
-                uint32_t tracks = (ch.GetByte() << 8) + ch.GetByte();
-                ticksPerQuarterNote = (ch.GetByte() << 8) + ch.GetByte();
+                uint32_t no_tracks = (ch.GetByte() << 8) + ch.GetByte();
+                PPQ = (ch.GetByte() << 8) + ch.GetByte();
 
-                LOG("format %i, tracks %i, ticksPerQuarterNote %i\n", format, tracks, ticksPerQuarterNote);
+                LOG("format %i, tracks %i, PPQ %i\n", format, no_tracks, PPQ);
             }
 
-            while (ch.done() == false)
+            while (no_tracks < 50 && ch.done() == false)
             {
                 if (ch.GetByte() == 'M' && ch.GetByte() == 'T' &&
                     ch.GetByte() == 'r' && ch.GetByte() == 'k')
                 {
                     int length = ch.GetLength(4);
-                    pTrack[tracks] = new MidiTrack(tracks, ch.m_pTrack, length);
+                    track[no_tracks] = new MidiTrack(no_tracks, ch.track, PPQ);
                     ch.Skip(length);
-                    tracks++;
+                    no_tracks++;
                 }
             }
         }
@@ -400,12 +505,34 @@ public:
         return midi.size();
     }
 
+    bool render(aax::AeonWave &aax)
+    {
+       static const float dt = 1e-3f;
+       _aaxTimer *timer = _aaxTimerCreate();
+       _aaxTimerStartRepeatable(timer, dt*1000000);
+       float time = 0.0f;
+
+       for(;;)
+       {
+          for (unsigned int t=0; t<no_tracks; ++t) {
+             track[t]->play(time);
+          }
+          time += dt;
+          _aaxTimerWait(timer);
+       }
+       _aaxTimerDestroy(timer);
+
+       return true;
+    }
+
 private:
     std::vector<unsigned char> midi;
 
-    MidiTrack *pTrack[50];
-    uint32_t tracks;
+    MidiTrack *track[50];
+    uint32_t no_tracks;
     float time;
+ 
+    uint32_t PPQ = 24; // ticks per quarter note
 };
 
 
@@ -413,14 +540,20 @@ int main(int argc, char **argv)
 {
     char *infile = getInputFile(argc, argv, IFILE_PATH);
 
-    Midi midi(infile);
-    if (midi)
+    if (getCommandLineOption(argc, argv, "-v") ||
+        getCommandLineOption(argc, argv, "--verbose"))
+    {
+       verbose = 1;
+    }
+
+    Midi file(infile);
+    if (file)
     {
         aax::AeonWave aax(AAX_MODE_WRITE_STEREO);
         aax.set(AAX_INITIALIZED);
         aax.set(AAX_PLAYING);
 
-        while(midi.render(aax)) {}
+        while(file.render(aax)) {}
 
         aax.set(AAX_PROCESSED);
     }
