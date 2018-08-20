829c24e3e26624b88e8743d07839ac8ca57e48f1
Erik Hofman <erik@ehofman.com>
2018-08-20 13:04:24 +0200

    Max pitch can go to 4.0

--- src/dsp/effect_pitch.c
+++ src/dsp/effect_pitch.c
@@ -107,7 +107,7 @@ _aaxPitchEffectMinMax(float val, int slot, unsigned char param)
 {
    static const _eff_minmax_tbl_t _aaxPitchRange[_MAX_FE_SLOTS] =
    {    /* min[4] */                  /* max[4] */
-    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 2.0f, 2.0f, 0.0f, 0.0f } },
+    { { 0.0f, 0.0f, 0.0f, 0.0f }, { 2.0f, 4.0f, 0.0f, 0.0f } },
     { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } },
     { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } },
     { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f } }
