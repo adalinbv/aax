677bcd98865f4b3d8953a2b2b7864c3bd01b93fd
Erik Hofman <erik@ehofman.com>
2018-12-04 10:42:45 +0100

    Update to the latest version

--- CMakeModules/FindXML.cmake
+++ CMakeModules/FindXML.cmake
@@ -1,4 +1,4 @@
-# Locate ZeroXML
+# Locate XML
 # This module defines
 # XML_LIBRARIES
 # XML_FOUND, if false, do not try to link to XML 
@@ -10,12 +10,13 @@
 #
 # Created by Erik Hofman.
 
+set(ProgramFilesx86 "PROGRAMFILES(x86)")
+
 FIND_PATH(XML_INCLUDE_DIR xml.h
   HINTS
   $ENV{XMLDIR}
-  $ENV{ProgramFiles}/ZeroXML
-  $ENV{ProgramFiles}/Adalin/ZeroXML
-  ${CMAKE_SOURCE_DIR}/zeroxml
+  $ENV{PROGRAMFILES}/ZeroXML
+  $ENV{${ProgramFilesx86}}/ZeroXML
   PATH_SUFFIXES include
   PATHS
   ~/Library/Frameworks
@@ -25,35 +26,46 @@ FIND_PATH(XML_INCLUDE_DIR xml.h
   /opt
 )
 
-FIND_LIBRARY(XML_LIBRARY 
-  NAMES XML xml zeroxml libzeroxml
-  HINTS
-  $ENV{XMLDIR}
-  $ENV{ProgramFiles}/ZeroXML
-  $ENV{ProgramFiles}/Adalin/ZeroXML
-  ${CMAKE_BUILD_DIR}/xml
-  PATH_SUFFIXES lib64 lib lib/${CMAKE_LIBRARY_ARCHITECTURE} libs64 libs libs/Win32 libs/Win64 bin
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /opt
-)
+IF(RMALLOC)
+  FIND_LIBRARY(XML_LIBRARY 
+    NAMES XML-rmalloc zeroxml-rmalloc ZeroXML-rmalloc libZeroXML-rmalloc
+    HINTS
+    $ENV{XMLDIR}
+    $ENV{ProgramFiles}/ZeroXML
+    $ENV{${ProgramFilesx86}}/ZeroXML
+    PATH_SUFFIXES lib64
+    PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /opt
+  )
 
+  CHECK_INCLUDE_FILE(${XML_INCLUDE_DIR}/rmalloc.h HAVE_RMALLOC_H)
+  SET(USE_RMALLOC 1)
+  SET(MALLOC_DEBUG ${USE_RMALLOC})
+
+
+ELSE(RMALLOC)
+  FIND_LIBRARY(XML_LIBRARY 
+    NAMES XML zeroxml ZeroXML libZeroXML
+    HINTS
+    $ENV{XMLDIR}
+    $ENV{ProgramFiles}/ZeroXML
+    $ENV{${ProgramFilesx86}}/ZeroXML
+    PATH_SUFFIXES lib64
+    PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /opt
+  )
+ENDIF(RMALLOC)
+
+SET(XML_FOUND "NO")
 IF(XML_LIBRARY AND XML_INCLUDE_DIR)
+  include_directories(${XML_INCLUDE_DIR})
   SET(XML_FOUND "YES")
-ELSE(XML_LIBRARY AND XML_INCLUDE_DIR)
-  IF(NOT XML_INCLUDE_DIR)
-    MESSAGE(FATAL_ERROR "Unable to find the XML library development files.")
-    SET(XML_FOUND "NO")
-  ENDIF(NOT XML_INCLUDE_DIR)
-  IF(NOT XML_LIBRARY)
-    IF(SINGLE_PACKAGE)
-      SET(XML_LIBRARY "${xml_BUILD_DIR}/xml/ZeroXML.lib")
-      SET(XML_FOUND "YES")
-    ELSE(SINGLE_PACKAGE)
-    ENDIF(SINGLE_PACKAGE)
-  ENDIF(NOT XML_LIBRARY)
 ENDIF(XML_LIBRARY AND XML_INCLUDE_DIR)
-
