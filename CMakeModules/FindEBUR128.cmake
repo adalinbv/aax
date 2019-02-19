b8b3376da11a6ecceae63b729516c6f22e2ddfac
Erik Hofman <erik@ehofman.com>
2019-02-19 12:46:37 +0100

    Windows fixes

--- CMakeModules/FindEBUR128.cmake
+++ CMakeModules/FindEBUR128.cmake
@@ -6,9 +6,21 @@
 # EBUR128_INCLUDE_DIR
 # EBUR128_LIBRARY
 
-find_path(EBUR128_INCLUDE_DIR NAMES ebur128.h)
+set(ProgramFilesx86 "ProgramFiles(x86)")
 
-find_library(EBUR128_LIBRARY NAMES ebur128)
+find_path(EBUR128_INCLUDE_DIR
+  NAMES ebur128.h
+  HINTS
+  $ENV{ProgramFilesx86}/libebur128
+  PATH_SUFFIXES include
+)
+
+find_library(EBUR128_LIBRARY
+  NAMES ebur128
+  HINTS
+  $ENV{ProgramFilesx86}/libebur128
+  PATH_SUFFIXES lib
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EBUR128 DEFAULT_MSG EBUR128_LIBRARY EBUR128_INCLUDE_DIR)
