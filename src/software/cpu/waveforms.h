84d90dad706a0283bada2e546e23769d51a88db6
Erik Hofman <erik@ehofman.com>
2019-07-21 22:46:01 +0200

    Add a mssing header

---
+++ src/software/cpu/waveforms.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2005-2019 by Erik Hofman.
+ * Copyright 2009-2019 by Adalin B.V.
+ *
+ * This file is part of AeonWave
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#ifndef WAVEFORMS_H
+#define WAVEFORMS_H 1
+
+
+// must be a multiple of 4 because of SIMD optimizations
+#define MAX_HARMONICS           16
+
+enum wave_types
+{
+   _CONSTANT_VALUE = 0,
+   _TRIANGLE_WAVE,
+   _SINE_WAVE,
+   _SQUARE_WAVE,
+   _SAWTOOTH_WAVE,
+   _IMPULSE_WAVE
+};
+
+extern float ALIGN _harmonics[AAX_MAX_WAVE][MAX_HARMONICS];
+
+#endif /* WAVEFORMS_H */
