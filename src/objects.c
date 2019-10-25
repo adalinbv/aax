2cb5df75d27cebf9283a7fc5cffd965e70833986
Erik Hofman <erik@ehofman.com>
2019-10-25 11:44:43 +0200

    use the proper enum

--- src/objects.c
+++ src/objects.c
@@ -515,8 +515,9 @@ _aaxGetFilterFromAAXS(aaxConfig config, const char *xid, float freq, float min, 
 
       src[slen] = 0;
       ftype = aaxFilterGetByName(config, src);
-      if (midi && (midi->mode == 2 ||
-                  (midi->mode == 1 && ftype != AAX_TIMED_GAIN_FILTER)))
+      if (midi && (midi->mode == AAX_RENDER_ARCADE ||
+                  (midi->mode == AAX_RENDER_SYNTHESIZER &&
+                   ftype != AAX_TIMED_GAIN_FILTER)))
       {
          return rv;
       }
@@ -619,9 +620,7 @@ _aaxGetEffectFromAAXS(aaxConfig config, const char *xid, float freq, float min, 
 
       src[slen] = 0;
       etype = aaxEffectGetByName(config, src);
-      if (midi && (midi->mode == 2 ||
-                  (midi->mode == 1 && etype != AAX_TIMED_PITCH_EFFECT)))
-      {
+      if (midi && midi->mode) {
          return rv;
       }
 
