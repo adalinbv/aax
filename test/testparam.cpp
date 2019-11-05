1bb11ace20134e113738c69bc88eae1e5f624f29
Erik Hofman <erik@ehofman.com>
2019-11-05 15:16:48 +0100

    Add a parameter test utility

---
+++ test/testparam.cpp
@@ -0,0 +1,45 @@
+
+#include <cstdio>
+#include <cstring>
+#include <aax/aeonwave.hpp>
+
+int main()
+{
+   aax::Param t, p = 3.1415927536f;
+   float v = 3.1415927536f;
+
+   if (p != v) printf("Error: %f != %f (should be equal)\n", float(p), v);
+
+   p += 1.0f;
+   if (p == v) printf("%f == %f (should be different)\n", float(p), v);
+   if (p != v) {} else printf("not %f != %f (should be different)\n", float(p), v);
+   if (p < v) printf("%f < %f (should be greater)\n", float(p), v);
+   if (p > v) {} else printf("not %f > %f (should be greater)\n", float(p), v);
+
+   v += 1.0f;
+   if (p != v) printf("inc: %f != %f (should be equal)\n", float(p), v);
+
+   p *= 7.5;
+   v *= 7.5f;
+   if (p != v) printf("mul: %f != %f (should be equal)\n", float(p), v);
+
+   p /= 0.33f;
+   v /= 0.33f;
+   if (p != v) printf("div: %f != %f (should be equal)\n", float(p), v);
+
+   p -= 1e3f;
+   v -= 1e3f;
+   if (p != v) printf("sub: %f != %f (should be equal)\n", float(p), v);
+
+   p += p;
+   v += v;
+   t = v;
+   if (p != t) printf("Param add: %f != %f (should be equal)\n", float(p), float(t));
+   if (p == t) {} else printf("Param add: not %f == %f (should be equal)\n", float(p), float(t));
+   if (p < t) printf("Param %f < %f (should be equal)\n", float(p), float(t));
+   p -= 0.5f;
+   if (p > t) printf("Param add: %f > %f (should be less)\n", float(p), float(t));
+   
+
+   return 0;
+}
