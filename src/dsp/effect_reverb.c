24ba8c719f820f920447c06d81da68b190aff638
Erik Hofman <erik@ehofman.com>
2020-01-07 14:04:20 +0100

    In case only 2nd order reverb is defined return AAX_TRUE

--- src/dsp/effect_reverb.c
+++ src/dsp/effect_reverb.c
@@ -839,6 +839,7 @@ _reverb_run(void *rb, MIX_PTR_T dptr, CONST_MIX_PTR_T sptr, MIX_PTR_T scratch,
             rbd->add(dptr, dpath, no_samples, 1.0f, 0.0f);
             memset(dpath, 0, reverb->no_samples*sizeof(MIX_T));
          }
+         if (!rv) rv = AAX_TRUE;
       }
    }
 
