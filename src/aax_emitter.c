38509b52f7b85edee30d51f6e2a3006e5e297bec
Erik Hofman <erik@ehofman.com>
2021-05-24 09:29:15 +0200

    Drive velocity past 1.0 to get a brighter sound at high velocity

--- src/aax_emitter.c
+++ src/aax_emitter.c
@@ -1124,8 +1124,8 @@ aaxEmitterSetSetup(aaxEmitter emitter, enum aaxSetupType type, unsigned int setu
       break;
    case AAX_VELOCITY_FACTOR:
    case AAX_MIDI_VELOCITY_FACTOR:
-      p2d->note.velocity = (float)setup/64.0f;			// 0.0 .. 2.0
-      p2d->note.velocity = 0.67f + 0.33f*p2d->note.velocity*p2d->note.velocity;
+      p2d->note.velocity = (float)setup/127.0f;			// 0.0 .. 2.0
+      p2d->note.velocity = 0.66f + 0.66f*p2d->note.velocity*p2d->note.velocity;
       break;
    case AAX_MIDI_SOFT_FACTOR:
       p2d->note.soft = (float)setup/127.0f;			// 0.0 .. 1.0
