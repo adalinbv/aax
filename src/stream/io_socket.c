bdff0039271178cfa546850f5169833f09606316
Erik Hofman <erik@ehofman.com>
2022-06-08 09:05:09 +0200

    Accept any version of libssl as long as they support the correct functions.

--- src/stream/io_socket.c
+++ src/stream/io_socket.c
@@ -101,7 +101,7 @@ _socket_open(_io_t *io, _data_t *buf, const char *remote, const char *pathname)
       }
 #endif
 
-      audio = _aaxIsLibraryPresent("ssl", "1.1");
+      audio = _aaxIsLibraryPresent("ssl", NULL);
       if (audio)
       {
          _aaxGetSymError(0);
