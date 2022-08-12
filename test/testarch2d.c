acb0443156233d81935c9f23abd5898c6fe4132a
Erik Hofman <erik@ehofman.com>
2022-08-12 11:24:31 +0200

    Fix a typo: test cubic resample

--- test/testarch2d.c
+++ test/testarch2d.c
@@ -576,7 +576,7 @@ int main()		// x86		ARM
        * resample
        * Cubic threshold is 0.25
        */
-      freq_factor = 0.8f;
+      freq_factor = 0.2f;
       _batch_resample_float = _batch_resample_float_cpu;
 
       printf("\n== Resample:\n");
