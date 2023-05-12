70d2c401c95b7084bb0f975385ad079ca4becc3a
Erik Hofman <erik@ehofman.com>
2023-05-12 14:04:32 +0200

    Udpated sound logo

--- src/sound_logo.h
+++ src/sound_logo.h
@@ -20,58 +20,66 @@
  */
 
 const char *
-__aax_sound_logo =  							\
-"<?xml version='1.0'?>"							\
-"<aeonwave>"								\
- "<sound frequency='220' db='-21' duration='3.0'>"				\
-  "<waveform src='triangle' pitch='0.51' voices='5' spread='0.9'/>"	\
-  "<waveform src='square' processing='modulate' ratio='0.8' pitch='8.'/>"\
-  "<waveform src='triangle' processing='add' ratio='0.3' pitch='0.94'/>"\
-  "<waveform src='pink-noise' processing='add' ratio='0.1' pitch='0.02'/>"\
-  "<filter type='frequency' src='48db'>"				\
-   "<slot>"								\
-    "<param>1650</param>"						\
-    "<param>1.0</param>"						\
-    "<param>0.1</param>"						\
-    "<param>3.0</param>"						\
-   "</slot>"								\
-  "</filter>"								\
- "</sound>"								\
- "<emitter looping='true'>"						\
-  "<filter type='timed-gain'>"						\
-   "<slot>"								\
-    "<param>0.5</param>"						\
-    "<param>0.41</param>"						\
-    "<param>1.0</param>"						\
-    "<param>2.525</param>"						\
-   "</slot>"								\
-   "<slot>"								\
-    "<param>0.8</param>"						\
-    "<param>2.25</param>"						\
-   "</slot>"								\
-  "</filter>"								\
-  "<effect type='phasing' src='inverse-sine'>"				\
-   "<slot>"								\
-    "<param>0.3</param>"						\
-    "<param>0.1</param>"						\
-    "<param>0.3</param>"						\
-    "<param>0.5</param>"						\
-   "</slot>"								\
-  "</effect>"								\
- "</emitter>"								\
- "<audioframe>"								\
-  "<filter type='frequency' src='sine' stereo='1'>"			\
-   "<slot>"								\
-    "<param>110</param>"						\
-    "<param>0.0</param>"						\
-    "<param>1.0</param>"						\
-    "<param>10.0</param>"						\
-   "</slot>"								\
-   "<slot>"								\
-    "<param>1100</param>"						\
-    "<param n='3'>0.06</param>"						\
-   "</slot>"								\
-  "</filter>"								\
- "</audioframe>"							\
+__aax_sound_logo =  							   \
+"<?xml version='1.0'?>"							   \
+"<aeonwave>"								   \
+ "<sound frequency='220' duration='3.0'>"				   \
+  "<waveform src='triangle' pitch='0.51' voices='5' spread='0.9'/>"	   \
+  "<waveform src='square' processing='modulate' ratio='0.8' pitch='8.'/>"  \
+  "<waveform src='triangle' processing='add' ratio='0.3' pitch='0.94'/>"   \
+  "<waveform src='pink-noise' processing='add' ratio='0.1' pitch='0.02'/>" \
+ "</sound>"								   \
+  "<emitter looping='true'>"						   \
+  "<filter type='frequency' src='48db'>"				   \
+   "<slot>"								   \
+    "<param n='0'>1650</param>"						   \
+    "<param n='1'>1.0</param>"						   \
+    "<param n='2'>0.1</param>"						   \
+    "<param n='3'>3.0</param>"						   \
+   "</slot>"								   \
+  "</filter>"								   \
+  "<filter type='timed-gain'>"						   \
+   "<slot>"								   \
+    "<param n='0'>0.5</param>"						   \
+    "<param n='1'>0.41</param>"						   \
+    "<param n='2'>1.0</param>"						   \
+    "<param n='3'>2.525</param>"					   \
+   "</slot>"								   \
+   "<slot>"								   \
+    "<param n='0'>0.8</param>"						   \
+    "<param n='1'>2.25</param>"						   \
+   "</slot>"								   \
+  "</filter>"								   \
+  "<effect type='phasing' src='inverse-sine'>"				   \
+   "<slot>"								   \
+    "<param n='0'>0.3</param>"						   \
+    "<param n='1'>0.1</param>"						   \
+    "<param n='2'>0.3</param>"						   \
+    "<param n='3'>0.5</param>"						   \
+   "</slot>"								   \
+  "</effect>"								   \
+ "</emitter>"								   \
+ "<audioframe>"								   \
+  "<filter type='frequency' src='sine' stereo='1'>"			   \
+   "<slot>"								   \
+    "<param n='0'>110</param>"						   \
+    "<param n='1'>0.0</param>"						   \
+    "<param n='2'>1.0</param>"						   \
+    "<param n='3'>10.0</param>"						   \
+   "</slot>"								   \
+   "<slot>"								   \
+    "<param n='0'>1100</param>"						   \
+    "<param n='3'>0.06</param>"						   \
+   "</slot>"								   \
+  "</filter>"								   \
+  "<effect type='reverb'>"						   \
+   "<slot>"								   \
+    "<param n='0'>571.9</param>"					   \
+    "<param n='1'>0.035</param>"					   \
+    "<param n='2'>0.504</param>"					   \
+    "<param n='3'>0.280</param>"					   \
+   "</slot>"								   \
+  "</effect>"								   \
+ "</audioframe>"							   \
 "</aeonwave>";
 
