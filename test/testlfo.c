c6725af8c4ebb009fd72af855afb99597b5f31ca
Erik Hofman <erik@ehofman.com>
2023-05-26 10:30:09 +0200

    Test all waveforms

--- test/testlfo.c
+++ test/testlfo.c
@@ -5,11 +5,14 @@
 #include "dsp/lfo.h"
 #include "api.h"
 
-#define TRY(a) if ((a) == 0) { printf("%i: %s\n", __LINE__, aaxGetErrorString(aaxGetErrorNo())); exit(-1); }
+#define TRY(a, d) if ((a) == 0) { \
+  printf("%i: %s %s\n", __LINE__, name, d); \
+  exit(-1); \
+}
 
-int main()
+void testLFO(enum aaxWaveformType state, const char *name)
 {
-    int constant, state;
+    int constant;
     _aaxMixerInfo info;
     _aaxLFOData *lfo;
     float period_rate;
@@ -18,9 +21,7 @@ int main()
     info.frequency = fs = 44100.0f;
     info.period_rate = period_rate = 90.0f;
 
-    TRY(lfo = _lfo_create());
-
-    state = AAX_SINE_WAVE;
+    TRY(lfo = _lfo_create(), "_lfo_create");
     _lfo_setup(lfo, &info, state);
 
     lfo->min = 0.0f;
@@ -28,18 +29,30 @@ int main()
 
     lfo->min_sec = lfo->min/lfo->fs;
     lfo->max_sec = lfo->max/lfo->fs;
-    lfo->f = 0.1f;
+    lfo->f = 5.0f;
 
     constant = _lfo_set_timing(lfo);
     lfo->envelope = AAX_FALSE;
 
-    TRY(_lfo_set_function(lfo, constant));
+    TRY(_lfo_set_function(lfo, constant), "_lfo_set_function");
 
+    printf("%s\n", name);
     for (f = 0.0f; f < 1.0f/lfo->f; f += 1.0f/period_rate)
     {
         n = lfo->get(lfo, NULL, NULL, 0, 0);
-        printf("%.1f - %.4f\n", f, n);
+        printf("%.1f ", n);
     }
+    printf("\n\n");
 
     _lfo_destroy(lfo);
+}
+
+int main()
+{
+    testLFO(AAX_IMPULSE_WAVE, "impulse");
+    testLFO(AAX_CYCLOID_WAVE, "cycloid");
+    testLFO(AAX_SINE_WAVE, "sine");
+    testLFO(AAX_TRIANGLE_WAVE, "triangle");
+    testLFO(AAX_SAWTOOTH_WAVE, "sawtooth");
+    testLFO(AAX_SQUARE_WAVE, "square");
 }
