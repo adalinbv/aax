f5375b2365d8a9a4fa7014bf3edeaa73a7b5747b
Erik Hofman <erik@ehofman.com>
2024-03-11 09:48:20 +0100

    slot->param[3] == 0.0f is perfectly valid

--- src/dsp/filter_volume.c
+++ src/dsp/filter_volume.c
@@ -215,8 +215,7 @@ _occlusion_create(_aaxRingBufferOcclusionData *occlusion, _aaxFilterInfo* slot,
    if (state != AAX_FALSE &&
        ((slot->param[0] >= 0.1f && slot->param[1] >= 0.1f) ||
         (slot->param[0] >= 0.1f && slot->param[2] >= 0.1f) ||
-        (slot->param[1] >= 0.1f && slot->param[2] >= 0.1f)) &&
-       slot->param[3] > LEVEL_64DB)
+        (slot->param[1] >= 0.1f && slot->param[2] >= 0.1f)))
    {
       if (!occlusion)
       {
