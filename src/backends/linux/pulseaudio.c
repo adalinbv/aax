adalinbv/aax

commit 4cd7683d58631568db893a9c77dd89f5988282e9

Fix a memory leak

src/backends/linux/pulseaudio.c
@@ -1141,6 +1141,10 @@ _aaxPulseAudioDriverGetDevices(const void *id, int mode)
             memmove(rv, s, MAX_DEVICES_LIST-sl);;
          }
       }
+
+      if (handle != id) {
+         free(handle);
+      }
    }
 
    return rv;

commit 9cf41526001b0335b0ba554b4901145ffe66c217

Fix a double free error introduced by 4cd7683d58631568db893a9c77dd89f5988282e9

src/backends/linux/pulseaudio.c
@@ -1089,6 +1089,7 @@ _aaxPulseAudioDriverGetDevices(const void *id, int mode)
    if (t_now > (t_previous[m]+5))
    {
       _driver_t *handle = (_driver_t *)id;
+      _driver_t *ptr = NULL;
 
       t_previous[m] = t_now;
       if (handle) {
@@ -1096,7 +1097,7 @@ _aaxPulseAudioDriverGetDevices(const void *id, int mode)
       }
       else
       {
-         handle = calloc(1, sizeof(_driver_t));
+         ptr = handle = calloc(1, sizeof(_driver_t));
          if (handle) {
             _aaxPulseAudioContextConnect(handle);
          }
@@ -1131,7 +1132,7 @@ _aaxPulseAudioDriverGetDevices(const void *id, int mode)
          }
          ppa_threaded_mainloop_unlock(handle->ml);
 
-         if (!id) {
+         if (ptr) {
             _aaxPulseAudioDriverDisconnect(handle);
          }
 
@@ -1140,10 +1141,6 @@ _aaxPulseAudioDriverGetDevices(const void *id, int mode)
          if (*s != '\0') {
             memmove(rv, s, MAX_DEVICES_LIST-sl);;
          }
-      }
-
-      if (handle != id) {
-         free(handle);
       }
    }
 