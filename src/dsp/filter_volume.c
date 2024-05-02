adalinbv/aax

commit c6e2ed2d7619b12d9e0faa1943097aed81097620

Combine almost all filter Destroy functions into aaxFilterDestroy and all effect Destroy functions in aaxEffectDestroy.

src/dsp/filter_volume.c
@@ -52,19 +52,6 @@ _aaxVolumeFilterCreate(_aaxMixerInfo *info, enum aaxFilterType type)
    return rv;
 }
 
-static int
-_aaxVolumeFilterDestroy(_filter_t* filter)
-{
-   if (filter->slot[0]->data)
-   {
-      filter->slot[0]->destroy(filter->slot[0]->data);
-      filter->slot[0]->data = NULL;
-   }
-   free(filter);
-
-   return true;
-}
-
 static aaxFilter
 _aaxVolumeFilterSetState(_filter_t* filter, int state)
 {
@@ -81,6 +68,7 @@ _aaxVolumeFilterSetState(_filter_t* filter, int state)
    else
    {
       filter->slot[0]->destroy(filter->slot[0]->data);
+      filter->slot[0]->data_size = 0;
       filter->slot[0]->data = NULL;
    }
 
@@ -152,14 +140,14 @@ _aaxVolumeFilterMinMax(float val, int slot, unsigned char param)
 _flt_function_tbl _aaxVolumeFilter =
 {
    "AAX_volume_filter_"AAX_MKSTR(VERSION), VERSION,
-   (_aaxFilterCreate*)&_aaxVolumeFilterCreate,
-   (_aaxFilterDestroy*)&_aaxVolumeFilterDestroy,
+   (_aaxFilterCreateFn*)&_aaxVolumeFilterCreate,
+   (_aaxFilterDestroyFn*)&_aaxFilterDestroy,
    NULL,
-   (_aaxFilterSetState*)&_aaxVolumeFilterSetState,
-   (_aaxNewFilterHandle*)&_aaxNewVolumeFilterHandle,
-   (_aaxFilterConvert*)&_aaxVolumeFilterSet,
-   (_aaxFilterConvert*)&_aaxVolumeFilterGet,
-   (_aaxFilterConvert*)&_aaxVolumeFilterMinMax
+   (_aaxFilterSetStateFn*)&_aaxVolumeFilterSetState,
+   (_aaxNewFilterHandleFn*)&_aaxNewVolumeFilterHandle,
+   (_aaxFilterConvertFn*)&_aaxVolumeFilterSet,
+   (_aaxFilterConvertFn*)&_aaxVolumeFilterGet,
+   (_aaxFilterConvertFn*)&_aaxVolumeFilterMinMax
 };
 
 
@@ -456,9 +444,10 @@ _occlusion_destroy(void *ptr)
    if (ptr)
    {
       _aaxRingBufferOcclusionData *occlusion = ptr;
+      _lfo_destroy(occlusion->freq_filter.lfo);
       _aax_aligned_free(occlusion->freq_filter.freqfilter);
+      _aax_aligned_free(occlusion);
    }
-   _aax_dsp_destroy(ptr);
 }
 
 int

src/aax_audioframe.c
@@ -166,6 +166,8 @@ aaxAudioFrameDestroy(aaxFrame frame)
       _FILTER_FREE_DATA(fmixer, EQUALIZER_LF);
       _aaxMutexDestroy(handle->mutex);
 
+      _FILTER_FREE_DATA(fmixer, EQUALIZER_LF);
+
       for (i=0; i<MAX_STEREO_FILTER; ++i) {
          _FILTER_FREE2D_DATA(fmixer, i);
       }

src/aax_filters.c
@@ -57,7 +57,7 @@ aaxFilterDestroy(aaxFilter f)
    if (filter)
    {
      _flt_function_tbl *flt = _aaxFilters[filter->type-1];
-      rv = flt->destroy(filter);
+      rv = flt->destroy(f);
    }
    return rv;
 }

src/dsp/common.c
@@ -28,8 +28,9 @@
 #include "arch.h"
 #include "common.h"
 
-void _aax_dsp_destroy(void *ptr) { if (ptr) _aax_aligned_free(ptr); }
-void _aax_dsp_aligned_destroy(void *ptr) { if (ptr) _aax_aligned_free(ptr); }
+void _aax_dsp_destroy(void *ptr) {
+    if (ptr) _aax_aligned_free(ptr);
+}
 void _aax_dsp_copy(void *d, void *s) {
    _aaxFilterInfo *dst = d, *src = s;
    dst->state = src->state;

src/dsp/common.h
@@ -149,8 +149,7 @@ typedef _aaxDSPInfo _aaxFilterInfo;
 typedef _aaxDSPInfo _aaxEffectInfo;
 
 
-void _aax_dsp_aligned_destroy(void *ptr);
-void _aax_dsp_destroy(void *ptr);
+void _aax_dsp_destroy(void*);
 void _aax_dsp_copy(void*, void*);
 void _aax_dsp_swap(void*, void*);
 

src/dsp/dsp_template.c
@@ -79,12 +79,16 @@ _aaxNewDSPHandle(const aaxConfig config, enum aaxDSPType type, _aax2dProps* p2d,
    return rv;
 }
 
+// Note: If your Destroy function looks exactly like the following code then
+// it's better to reference aaxFilteDestroy or aaxEffectDestroy in the
+// _dsp_function_tbl function table below.
 static int
 _aaxDSPDestroy(_dsp_t* dsp)
 {
    if (dsp->slot[0]->data)
    {
       dsp->slot[0]->destroy(dsp->slot[0]->data);
+      dsp->slot[0]->data_size = 0;
       dsp->slot[0]->data = NULL;
    }
    free(dsp);

src/dsp/effect_convolution.c
@@ -36,7 +36,7 @@ static int _convolution_run(const _aaxDriverBackend*, const void*, void*, void*)
 static aaxEffect
 _aaxConvolutionEffectCreate(_aaxMixerInfo *info, enum aaxEffectType type)
 {
-   _effect_t* eff = _aaxEffectCreateHandle(info, type, 2, DSIZE);
+   _effect_t* eff = _aaxEffectCreateHandle(info, type, 2, 0);
    aaxEffect rv = NULL;
 
    if (eff)
@@ -49,19 +49,6 @@ _aaxConvolutionEffectCreate(_aaxMixerInfo *info, enum aaxEffectType type)
       rv = (aaxEffect)eff;
    }
    return rv;
-}
-
-static int
-_aaxConvolutionEffectDestroy(_effect_t* effect)
-{
-   if (effect->slot[0]->data)
-   {
-      effect->slot[0]->destroy(effect->slot[0]->data);
-      effect->slot[0]->data = NULL;
-   }
-   free(effect);
-
-   return true;
 }
 
 static aaxEffect
@@ -154,6 +141,7 @@ _aaxConvolutionEffectSetState(_effect_t* effect, int state)
       if (effect->slot[0]->data)
       {
          effect->slot[0]->destroy(effect->slot[0]->data);
+         effect->slot[0]->data_size = 0;
          effect->slot[0]->data = NULL;
       }
       break;
@@ -173,7 +161,11 @@ _aaxConvolutionEffectSetData(_effect_t* effect, aaxBuffer buffer)
    {
       convolution = _aax_aligned_alloc(DSIZE);
       effect->slot[0]->data = convolution;
-      if (convolution) memset(convolution, 0, DSIZE);
+      if (convolution)
+      {
+         memset(convolution, 0, DSIZE);
+         effect->slot[0]->data_size = DSIZE;
+      }
    }
 
    if (convolution && info)
@@ -310,15 +302,15 @@ _aaxConvolutionEffectMinMax(float val, int slot, unsigned char param)
 _eff_function_tbl _aaxConvolutionEffect =
 {
    "AAX_convolution_effect", VERSION,
-   (_aaxEffectCreate*)&_aaxConvolutionEffectCreate,
-   (_aaxEffectDestroy*)&_aaxConvolutionEffectDestroy,
+   (_aaxEffectCreateFn*)&_aaxConvolutionEffectCreate,
+   (_aaxEffectDestroyFn*)&_aaxEffectDestroy,
    NULL,
-   (_aaxEffectSetState*)&_aaxConvolutionEffectSetState,
-   (_aaxEffectSetData*)&_aaxConvolutionEffectSetData,
-   (_aaxNewEffectHandle*)&_aaxNewConvolutionEffectHandle,
-   (_aaxEffectConvert*)&_aaxConvolutionEffectSet,
-   (_aaxEffectConvert*)&_aaxConvolutionEffectGet,
-   (_aaxEffectConvert*)&_aaxConvolutionEffectMinMax
+   (_aaxEffectSetStateFn*)&_aaxConvolutionEffectSetState,
+   (_aaxEffectSetDataFn*)&_aaxConvolutionEffectSetData,
+   (_aaxNewEffectHandleFn*)&_aaxNewConvolutionEffectHandle,
+   (_aaxEffectConvertFn*)&_aaxConvolutionEffectSet,
+   (_aaxEffectConvertFn*)&_aaxConvolutionEffectGet,
+   (_aaxEffectConvertFn*)&_aaxConvolutionEffectMinMax
 };
 
 void

src/dsp/effect_delay.c
@@ -66,19 +66,6 @@ _aaxDelayEffectCreate(_aaxMixerInfo *info, enum aaxEffectType type)
    return rv;
 }
 
-static int
-_aaxDelayEffectDestroy(_effect_t* effect)
-{
-   if (effect->slot[0]->data)
-   {
-      effect->slot[0]->destroy(effect->slot[0]->data);
-      effect->slot[0]->data = NULL;
-   }
-   free(effect);
-
-   return true;
-}
-
 static aaxEffect
 _aaxDelayEffectSetState(_effect_t* effect, int state, float delay_gain, float feedback_gain, float max_delay)
 {
@@ -130,7 +117,6 @@ _aaxDelayEffectSetState(_effect_t* effect, int state, float delay_gain, float fe
          float fs = 48000.0f;
          int t, constant;
 
-         effect->slot[0]->data_size = DSIZE;
          if (effect->info) {
             fs = effect->info->frequency;
          }
@@ -324,6 +310,7 @@ _aaxDelayEffectSetState(_effect_t* effect, int state, float delay_gain, float fe
       if (effect->slot[0]->data)
       {
          effect->slot[0]->destroy(effect->slot[0]->data);
+         effect->slot[0]->data_size = 0;
          effect->slot[0]->data = NULL;
       }
       break;
@@ -442,15 +429,15 @@ _aaxDelayEffectMinMax(float val, int slot, unsigned char param)
 _eff_function_tbl _aaxDelayLineEffect =
 {
    "AAX_delay_effect_"AAX_MKSTR(VERSION), VERSION,
-   (_aaxEffectCreate*)&_aaxDelayEffectCreate,
-   (_aaxEffectDestroy*)&_aaxDelayEffectDestroy,
-   (_aaxEffectReset*)&_delay_reset,
-   (_aaxEffectSetState*)&_aaxDelayLineEffectSetState,
+   (_aaxEffectCreateFn*)&_aaxDelayEffectCreate,
+   (_aaxEffectDestroyFn*)&_aaxEffectDestroy,
+   (_aaxEffectResetFn*)&_delay_reset,
+   (_aaxEffectSetStateFn*)&_aaxDelayLineEffectSetState,
    NULL,
-   (_aaxNewEffectHandle*)&_aaxNewDelayEffectHandle,
-   (_aaxEffectConvert*)&_aaxDelayEffectSet,
-   (_aaxEffectConvert*)&_aaxDelayEffectGet,
-   (_aaxEffectConvert*)&_aaxDelayEffectMinMax
+   (_aaxNewEffectHandleFn*)&_aaxNewDelayEffectHandle,
+   (_aaxEffectConvertFn*)&_aaxDelayEffectSet,
+   (_aaxEffectConvertFn*)&_aaxDelayEffectGet,
+   (_aaxEffectConvertFn*)&_aaxDelayEffectMinMax
 };
 
 /* -- Chorus ---------------------------------------------------------------- */
@@ -548,15 +535,15 @@ _aaxChorusEffectMinMax(float val, int slot, unsigned char param)
 _eff_function_tbl _aaxChorusEffect =
 {
    "AAX_chorus_effect_"AAX_MKSTR(VERSION), VERSION,
-   (_aaxEffectCreate*)&_aaxDelayEffectCreate,
-   (_aaxEffectDestroy*)&_aaxDelayEffectDestroy,
-   (_aaxEffectReset*)&_delay_reset,
-   (_aaxEffectSetState*)&_aaxChorusEffectSetState,
+   (_aaxEffectCreateFn*)&_aaxDelayEffectCreate,
+   (_aaxEffectDestroyFn*)&_aaxEffectDestroy,
+   (_aaxEffectResetFn*)&_delay_reset,
+   (_aaxEffectSetStateFn*)&_aaxChorusEffectSetState,
    NULL,
-   (_aaxNewEffectHandle*)&_aaxNewDelayEffectHandle,
-   (_aaxEffectConvert*)&_aaxChorusEffectSet,
-   (_aaxEffectConvert*)&_aaxChorusEffectGet,
-   (_aaxEffectConvert*)&_aaxChorusEffectMinMax
+   (_aaxNewEffectHandleFn*)&_aaxNewDelayEffectHandle,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectSet,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectGet,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectMinMax
 };
 
 /* -- Phasing --------------------------------------------------------------- */
@@ -624,15 +611,15 @@ _aaxPhasingEffectSet(float val, int ptype, unsigned char param)
 _eff_function_tbl _aaxPhasingEffect =
 {
    "AAX_phasing_effect_"AAX_MKSTR(VERSION), VERSION,
-   (_aaxEffectCreate*)&_aaxDelayEffectCreate,
-   (_aaxEffectDestroy*)&_aaxDelayEffectDestroy,
-   (_aaxEffectReset*)&_delay_reset,
-   (_aaxEffectSetState*)&_aaxChorusEffectSetState,
+   (_aaxEffectCreateFn*)&_aaxDelayEffectCreate,
+   (_aaxEffectDestroyFn*)&_aaxEffectDestroy,
+   (_aaxEffectResetFn*)&_delay_reset,
+   (_aaxEffectSetStateFn*)&_aaxChorusEffectSetState,
    NULL,
-   (_aaxNewEffectHandle*)&_aaxNewDelayEffectHandle,
-   (_aaxEffectConvert*)&_aaxPhasingEffectSet,
-   (_aaxEffectConvert*)&_aaxPhasingEffectGet,
-   (_aaxEffectConvert*)&_aaxChorusEffectMinMax
+   (_aaxNewEffectHandleFn*)&_aaxNewDelayEffectHandle,
+   (_aaxEffectConvertFn*)&_aaxPhasingEffectSet,
+   (_aaxEffectConvertFn*)&_aaxPhasingEffectGet,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectMinMax
 };
 
 /* -- Flanger --------------------------------------------------------------- */
@@ -653,15 +640,15 @@ _aaxFlangingEffectSetState(_effect_t* effect, int state)
 _eff_function_tbl _aaxFlangingEffect =
 {
    "AAX_flanging_effect_"AAX_MKSTR(VERSION), VERSION,
-   (_aaxEffectCreate*)&_aaxDelayEffectCreate,
-   (_aaxEffectDestroy*)&_aaxDelayEffectDestroy,
-   (_aaxEffectReset*)&_delay_reset,
-   (_aaxEffectSetState*)&_aaxFlangingEffectSetState,
+   (_aaxEffectCreateFn*)&_aaxDelayEffectCreate,
+   (_aaxEffectDestroyFn*)&_aaxEffectDestroy,
+   (_aaxEffectResetFn*)&_delay_reset,
+   (_aaxEffectSetStateFn*)&_aaxFlangingEffectSetState,
    NULL,
-   (_aaxNewEffectHandle*)&_aaxNewDelayEffectHandle,
-   (_aaxEffectConvert*)&_aaxChorusEffectSet,
-   (_aaxEffectConvert*)&_aaxChorusEffectGet,
-   (_aaxEffectConvert*)&_aaxChorusEffectMinMax
+   (_aaxNewEffectHandleFn*)&_aaxNewDelayEffectHandle,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectSet,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectGet,
+   (_aaxEffectConvertFn*)&_aaxChorusEffectMinMax
 };
 
 /* -------------------------------------------------------------------------- */
@@ -678,7 +665,7 @@ _delay_create(void *d, void *i, bool feedback, int state, float delay_time)
 
    if (data == NULL)
    {
-      data  = _aax_aligned_alloc(DSIZE);
+      data = _aax_aligned_alloc(DSIZE);
       if (data) memset(data, 0, DSIZE);
    }
 
@@ -817,8 +804,8 @@ _delay_destroy(void *ptr)
          _freqfilter_destroy(data->freq_filter);
          data->freq_filter = NULL;
       }
+      _aax_aligned_free(data);
    }
-   _aax_dsp_destroy(ptr);
 }
 
 static void

commit bd3acf858829fac56af422732df55aab1dcd6d23

Fix a double free error introduced by c6e2ed2d7619b12d9e0faa1943097aed81097620

src/dsp/filter_volume.c
@@ -46,7 +46,7 @@ _aaxVolumeFilterCreate(_aaxMixerInfo *info, enum aaxFilterType type)
       _aaxSetDefaultFilter2d(flt->slot[0], flt->pos, 0);
       _aaxSetDefaultFilter3d(flt->slot[1], flt->pos, 1);
       flt->slot[0]->destroy = _occlusion_destroy;
-      flt->slot[0]->swap = _aax_dsp_swap;
+      flt->slot[0]->swap = _occlusion_swap;
       rv = (aaxFilter)flt;
    }
    return rv;
@@ -86,12 +86,12 @@ _aaxNewVolumeFilterHandle(const aaxConfig config, enum aaxFilterType type, _aax2
    {
       _aaxRingBufferOcclusionData *occlusion;
 
-      occlusion = (_aaxRingBufferOcclusionData*)p3d->effect[rv->pos].data;
+      occlusion = (_aaxRingBufferOcclusionData*)p2d->effect[rv->pos].data;
       _occlusion_to_effect(rv->slot[1], occlusion);
 
       _aax_dsp_copy(rv->slot[0], &p2d->filter[rv->pos]);
       rv->slot[0]->destroy = _occlusion_destroy;
-      rv->slot[0]->swap = _aax_dsp_swap;
+      rv->slot[0]->swap = _occlusion_swap;
 
       rv->state = p3d->filter[rv->pos].state;
    }
@@ -422,6 +422,21 @@ _occlusion_prepare(_aaxEmitter *src, const _aax3dProps *fp3d, void *data)
       }
       while (nfp3d);
    } /* pdp3d_m != NULL */
+}
+
+void
+_occlusion_swap(void *d, void *s)
+{
+   _aaxRingBufferConvolutionData *docc;
+   _aaxFilterInfo *dst = d;
+   void *ffhist = NULL;
+
+   docc = dst->data;
+   if (docc) ffhist = docc->freq_filter->freqfilter;
+
+   _aax_dsp_swap(d, s);
+
+   if (ffhist) docc->freq_filter->freqfilter = ffhist;
 }
 
 void