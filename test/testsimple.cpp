12bb4f8af4f0f6be9db95bd4ff5158ed07def5c6
Erik Hofman <erik@ehofman.com>
2024-10-28 14:22:17 +0100

    Calling mixer.source() with a name which is already in use, but with a different sound file, will replace the previous occurence of the soundsource

--- test/testsimple.cpp
+++ test/testsimple.cpp
@@ -9,8 +9,13 @@ int main()
     aeonwave::SimpleMixer mixer;
 
     auto& source = mixer.source("sine", SRC_PATH"/test-sine.aaxs");
-
-    source.play(2.0f);
+    source.set_volume(0.7f);
+    source.set_pitch(2.0f);
+    source.play();
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    for(int i=0; i<100; ++i) {
+        source.set_pitch(2.0f - 0.01f*i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(15));
+    }
     source.stop();
 };
